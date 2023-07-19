627f228932d0b75ba9db5a1a280d860ddc5cd2de
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2023-07-19 18:15:33 -0400

    feat: account creation working, need to update signing

--- crates/banyan-core-service/src/api/auth/handlers.rs
+++ crates/banyan-core-service/src/api/auth/handlers.rs
@@ -1,34 +1,71 @@
-use axum::extract;
+use axum::extract::{self, Json};
 use axum::http::StatusCode;
 use axum::response::{IntoResponse, Response};
 use jsonwebtoken::{encode, get_current_timestamp, Header};
-use uuid::Uuid;
+use serde::Serialize;
+use sqlx::FromRow;
 
-use crate::api::ErrorResponse;
-use crate::api::auth::AuthError;
 use crate::api::auth::requests::RegisterDeviceKey;
-use crate::extractors::{ApiToken, EXPIRATION_WINDOW_SECS, SigningKey};
+use crate::api::auth::AuthError;
+use crate::api::ErrorResponse;
+use crate::extractors::{ApiToken, DbConn, SigningKey};
 
-pub async fn fake_register(signing_key: SigningKey) -> Response {
+const FAKE_REGISTRATION_MAXIMUM_DURATION: u64 = 60 * 60 * 24 * 28; // four weeks, should be good
+                                                                   // enough for any development
+#[derive(FromRow)]
+struct CreatedAccount {
+    id: String,
+}
+
+#[derive(Serialize)]
+struct NewAccount {
+    id: String,
+    token: String,
+}
+
+pub async fn fake_register(mut db_conn: DbConn, signing_key: SigningKey) -> Response {
+    let maybe_account = sqlx::query_as!(
+        CreatedAccount,
+        r#"INSERT INTO accounts DEFAULT VALUES RETURNING id;"#
+    )
+    .fetch_one(&mut *db_conn.0)
+    .await;
+
+    let created_account = match maybe_account {
+        Ok(ca) => ca,
+        Err(_) => {
+            return (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "unable to create new account",
+            )
+                .into_response();
+        }
+    };
+
     let api_token = ApiToken {
-        nonce: Some("todo-generate-random-none".to_string()),
+        nonce: Some(generate_nonce()),
 
-        expiration: get_current_timestamp() + EXPIRATION_WINDOW_SECS,
+        expiration: get_current_timestamp() + FAKE_REGISTRATION_MAXIMUM_DURATION,
         not_before: get_current_timestamp(),
 
-        audience: "did:key:{some-kind-of-banyan-identity}".into(),
-        subject: Uuid::new_v4().to_string(),
+        audience: "banyan-platform".into(),
+        subject: created_account.id.clone(),
     };
 
-    let token_header = Header {
-        kid: Some("4e:12:43:bd:22:c6:6e:76:c2:ba:9e:dd:c1:f9:13:94:e5:7f:9f:83".to_string()),
-        ..Default::default()
-    };
-
-    match encode(&token_header, &api_token, &signing_key.0) {
-        Ok(token_contents) => (StatusCode::OK, token_contents).into_response(),
-        Err(_) => ErrorResponse::from(AuthError).into_response(),
+    if let Err(err) = encode(&Header::default(), &api_token, &signing_key.0) {
+        tracing::error!("error: {err}");
     }
+
+    //match encode(&Header::default(), &api_token, &signing_key.0) {
+    //    Ok(token_contents) => Json(NewAccount {
+    //        id: uuid,
+    //        token: token_contents,
+    //    })
+    //    .into_response(),
+    //    Err(_) => ErrorResponse::from(AuthError).into_response(),
+    //}
+
+    Json(NewAccount { id: created_account.id, token: "test token".to_string() }).into_response()
 }
 
 pub async fn register_device_key(
@@ -40,5 +77,11 @@ pub async fn register_device_key(
 }
 
 fn generate_nonce() -> String {
-    todo!()
+    use rand::Rng;
+    let mut rng = rand::thread_rng();
+
+    (0..8)
+        .map(|_| rng.gen_range(0..256))
+        .map(|b| format!("{:02x}", b))
+        .collect()
 }
--- crates/banyan-core-service/src/app_state.rs
+++ crates/banyan-core-service/src/app_state.rs
@@ -3,9 +3,9 @@ use std::path::PathBuf;
 use axum::extract::FromRef;
 use jsonwebtoken::{DecodingKey, EncodingKey};
 use object_store::local::LocalFileSystem;
-use sqlx::sqlite::SqlitePool;
 use ring::rand::SystemRandom;
-use ring::signature::{ECDSA_P256_SHA256_FIXED_SIGNING, EcdsaKeyPair, KeyPair};
+use ring::signature::{EcdsaKeyPair, KeyPair, ECDSA_P256_SHA256_FIXED_SIGNING};
+use sqlx::sqlite::SqlitePool;
 
 mod database;
 mod state_error;
@@ -16,8 +16,10 @@ pub use state_error::StateError;
 #[derive(Clone)]
 pub struct AppState {
     database_pool: SqlitePool,
+
     signing_key: EncodingKey,
     verification_key: DecodingKey,
+
     upload_directory: PathBuf,
 }
 
@@ -30,7 +32,8 @@ impl AppState {
             .map_err(StateError::inaccessible_upload_directory)?;
 
         let database_pool = database::setup(config.database_url()).await?;
-        let (signing_key, verification_key) = load_or_create_service_key(config.signing_key_path())?;
+        let (signing_key, verification_key) =
+            load_or_create_service_key(config.signing_key_path())?;
 
         Ok(Self {
             database_pool,
@@ -71,14 +74,13 @@ fn load_or_create_service_key(path: &PathBuf) -> Result<(EncodingKey, DecodingKe
 
         let pem = pem::Pem::new("ECDSA PRIVATE KEY".to_string(), key.as_ref());
 
-        std::fs::write(path, pem::encode(&pem))
-            .map_err(StateError::write_service_key)?;
+        std::fs::write(path, pem::encode(&pem)).map_err(StateError::write_service_key)?;
     };
 
-    let service_key_bytes = std::fs::read(path)
-        .map_err(StateError::read_service_key)?;
+    let service_key_bytes = std::fs::read(path).map_err(StateError::read_service_key)?;
 
-    let service_key_pem = pem::parse(service_key_bytes).map_err(|_| StateError::invalid_service_key())?;
+    let service_key_pem =
+        pem::parse(service_key_bytes).map_err(|_| StateError::invalid_service_key())?;
 
     if service_key_pem.tag() != "ECDSA PRIVATE KEY" {
         return Err(StateError::invalid_service_key());
@@ -86,8 +88,11 @@ fn load_or_create_service_key(path: &PathBuf) -> Result<(EncodingKey, DecodingKe
 
     let private_key_der_bytes = service_key_pem.into_contents();
 
-    let ekp = EcdsaKeyPair::from_pkcs8(&ECDSA_P256_SHA256_FIXED_SIGNING, private_key_der_bytes.as_ref())
-        .map_err(|_| StateError::invalid_service_key())?;
+    let ekp = EcdsaKeyPair::from_pkcs8(
+        &ECDSA_P256_SHA256_FIXED_SIGNING,
+        private_key_der_bytes.as_ref(),
+    )
+    .map_err(|_| StateError::invalid_service_key())?;
 
     let encoding_key = EncodingKey::from_ec_der(private_key_der_bytes.as_ref());
     let decoding_key = DecodingKey::from_ec_der(ekp.public_key().as_ref());
