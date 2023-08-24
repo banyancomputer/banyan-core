32833e6f2c3d0d58a98af3151a3f1d9a874c803d
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2023-08-24 18:01:58 -0400

    feat: better error handling on the upload store extractor

--- crates/banyan-staging-service/src/middleware/upload_store.rs
+++ crates/banyan-staging-service/src/middleware/upload_store.rs
@@ -1,9 +1,10 @@
 use std::ops::Deref;
 
-use axum::async_trait;
+use axum::{async_trait, Json};
 use axum::extract::FromRequestParts;
 use axum::http::request::Parts;
 use axum::http::StatusCode;
+use axum::response::{IntoResponse, Response};
 use object_store::local::LocalFileSystem;
 
 use crate::app::State;
@@ -21,7 +22,7 @@ impl Deref for UploadStore {
 
 #[async_trait]
 impl FromRequestParts<State> for UploadStore {
-    type Rejection = (StatusCode, String);
+    type Rejection = UploadStoreError;
 
     async fn from_request_parts(
         _parts: &mut Parts,
@@ -29,9 +30,29 @@ impl FromRequestParts<State> for UploadStore {
     ) -> Result<Self, Self::Rejection> {
         let store = match LocalFileSystem::new_with_prefix(state.upload_directory()) {
             Ok(s) => s,
-            Err(err) => return Err((StatusCode::INTERNAL_SERVER_ERROR, err.to_string())),
+            Err(err) => return Err(UploadStoreError::DirectoryUnavailable(err)),
         };
 
         Ok(Self(store))
+    }
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum UploadStoreError {
+    #[error("unable to access upload directory")]
+    DirectoryUnavailable(object_store::Error),
+}
+
+impl IntoResponse for UploadStoreError {
+    fn into_response(self) -> Response {
+        use UploadStoreError::*;
+
+        match self {
+            DirectoryUnavailable(err) => {
+                tracing::error!(err = ?err, "upload directory was unavailable for request");
+                let err_msg = serde_json::json!({ "msg": "data uploads currently unavailable due to service error" });
+                (StatusCode::INTERNAL_SERVER_ERROR, Json(err_msg)).into_response()
+            }
+        }
     }
 }
