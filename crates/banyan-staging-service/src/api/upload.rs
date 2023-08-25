0c1f5dcdaf3079f310e75ca8ebd7485e85cc8cbe
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2023-08-24 22:32:07 -0400

    feat: stub in some of the work aroudn authenticated client

--- crates/banyan-staging-service/src/api/upload.rs
+++ crates/banyan-staging-service/src/api/upload.rs
@@ -1,8 +1,9 @@
 use axum::http::StatusCode;
 use axum::response::{IntoResponse, Response};
 
+use crate::extractors::AuthenticatedClient;
 use crate::extractors::UploadStore;
 
-pub async fn handler(_store: UploadStore) -> Response {
+pub async fn handler(_client: AuthenticatedClient, _store: UploadStore) -> Response {
     (StatusCode::NO_CONTENT, ()).into_response()
 }
--- crates/banyan-staging-service/src/extractors/authenticated_client.rs
+++ crates/banyan-staging-service/src/extractors/authenticated_client.rs
@@ -1,1 +1,61 @@
-pub struct AuthenticatedClient;
+use std::collections::HashSet;
+use std::sync::OnceLock;
+
+use axum::{async_trait, Json, RequestPartsExt};
+use axum::extract::{FromRef, FromRequestParts, TypedHeader};
+use axum::extract::rejection::TypedHeaderRejection;
+use axum::headers::Authorization;
+use axum::headers::authorization::Bearer;
+use axum::http::StatusCode;
+use axum::response::{IntoResponse, Response};
+use http::request::Parts;
+use jwt_simple::prelude::*;
+use regex::Regex;
+use uuid::Uuid;
+
+use crate::database::Db;
+
+/// Defines the maximum length of time we consider any individual token valid in seconds. If the
+/// expiration is still in the future, but it was issued more than this many seconds in the past
+/// we'll reject the token even if its otherwise valid.
+const MAXIMUM_TOKEN_AGE: u64 = 900;
+
+static KEY_ID_PATTERN: &str = r"^([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})@([0-9a-f]{2}(:[0-9a-f]{2}){19})$";
+
+pub struct AuthenticatedClient {
+    device_id: Uuid,
+    fingerprint: String,
+}
+
+#[async_trait]
+impl<S> FromRequestParts<S> for AuthenticatedClient
+where
+    Db: FromRef<S>,
+    S: Send + Sync,
+{
+    type Rejection = AuthenticatedClientError;
+
+    async fn from_request_parts(_parts: &mut Parts, _state: &S) -> Result<Self, Self::Rejection> {
+        Err(AuthenticatedClientError::Placeholder)
+    }
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum AuthenticatedClientError {
+    #[error("placeholder till the code is fleshed out")]
+    Placeholder,
+}
+
+impl IntoResponse for AuthenticatedClientError {
+    fn into_response(self) -> Response {
+        use AuthenticatedClientError::*;
+
+        match self {
+            Placeholder => {
+                tracing::error!("a placeholder error occurred extracting the authenticated client");
+                let err_msg = serde_json::json!({ "msg": "here there be dragons" });
+                (StatusCode::INTERNAL_SERVER_ERROR, Json(err_msg)).into_response()
+            }
+        }
+    }
+}
