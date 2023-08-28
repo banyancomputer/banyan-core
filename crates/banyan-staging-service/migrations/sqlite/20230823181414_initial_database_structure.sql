c8acc822eb8aaacc930b343823f9d57d015ad873
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2023-08-28 15:27:18 -0400

    fix: syntax in sql

--- crates/banyan-staging-service/migrations/sqlite/20230823181414_initial_database_structure.sql
+++ crates/banyan-staging-service/migrations/sqlite/20230823181414_initial_database_structure.sql
@@ -11,7 +11,7 @@ CREATE TABLE clients (
   fingerprint VARCHAR(64) NOT NULL,
   public_key TEXT NOT NULL,
 
-  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE UNIQUE INDEX idx_clients_on_fingerprint
@@ -30,7 +30,7 @@ CREATE TABLE storage_grants (
   remote_grant_id TEXT NOT NULL,
   allowed_storage INT NOT NULL DEFAULT 0,
 
-  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE UNIQUE INDEX idx_storage_grants_on_remote_grant_id
@@ -48,21 +48,18 @@ CREATE TABLE uploads (
   client_id TEXT NOT NULL REFERENCES clients(id),
   metadata_id TEXT NOT NULL,
 
-  reported_size INTEGER NOT NULL CONSTRAINT reported_size_positive (reported_size >= 0),
-  final_size INTEGER NOT NULL CONSTRAINT final_size_positive (reported_size >= 0),
+  reported_size INTEGER NOT NULL CHECK (reported_size >= 0) CONSTRAINT reported_size_positive,
+  final_size INTEGER NOT NULL CHECK (reported_size >= 0) CONSTRAINT final_size_positive,
 
   file_path VARCHAR(128) NOT NULL,
-  state VARCHAR(32) NOT NULL
-    CONSTRAINT state_in_list (
-      state IN ('started', 'failed', 'indexing', 'complete')
-    ),
+  state VARCHAR(32) NOT NULL CHECK (state IN ('started', 'indexing', 'complete', 'failed')) CONSTRAINT state_in_list,
 
   started_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  finished_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  finished_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE UNIQUE INDEX idx_blocks_on_cid
-  ON blocks(cid);
+CREATE UNIQUE INDEX idx_uploads_on_metadata_id
+  ON uploads(metadata_id);
 
 CREATE TABLE blocks (
   id TEXT NOT NULL PRIMARY KEY DEFAULT (
@@ -75,7 +72,7 @@ CREATE TABLE blocks (
 
   cid VARCHAR(64) NOT NULL,
 
-  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE UNIQUE INDEX idx_blocks_on_cid
@@ -85,10 +82,10 @@ CREATE TABLE uploads_blocks (
   upload_id TEXT NOT NULL REFERENCES uploads(id),
   block_id TEXT NOT NULL REFERENCES blocks(id),
 
-  byte_offset INTEGER NOT NULL CONSTRAINT byte_offset_positive (byte_offset >= 0),
-  data_length: INTEGER NOT NULL CONSTRAINT data_length_positive (data_length >= 0),
+  byte_offset INTEGER NOT NULL CHECK (byte_offset >= 0) CONSTRAINT byte_offset_positive,
+  data_length INTEGER NOT NULL CHECK (data_length >= 0) CONSTRAINT data_length_positive,
 
-  associated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  associated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE UNIQUE INDEX idx_uploads_blocks_on_upload_id_block_id
