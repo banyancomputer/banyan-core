05746c6ba6b2a9ed0a6d7c3fbc8e968857b35413
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2023-08-28 15:51:18 -0400

    fix: more testing and sql fixes

--- crates/banyan-staging-service/migrations/postgres/20230823181414_initial_database_structure.sql
+++ crates/banyan-staging-service/migrations/postgres/20230823181414_initial_database_structure.sql
@@ -20,7 +20,7 @@ CREATE TABLE storage_grants (
   remote_grant_id UUID NOT NULL,
   allowed_storage INTEGER NOT NULL DEFAULT 0 CHECK (allowed_storage > 0),
 
-  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE UNIQUE INDEX idx_storage_grants_on_remote_grant_id
