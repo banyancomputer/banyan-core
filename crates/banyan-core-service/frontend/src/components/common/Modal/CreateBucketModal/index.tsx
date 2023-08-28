61d84bc3b756ff30017be564eafbcd8299ae161d
vladdezhniuk <vladdezhniuk@gmail.com>
2023-08-28 15:30:37 +0300

    feat: custom select momponent implemented, add bucket modal added, add folder modal added

---
+++ crates/banyan-core-service/frontend/src/components/common/Modal/CreateBucketModal/index.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import { useIntl } from 'react-intl';
+import { MdDone } from 'react-icons/md';
+
+import { useModal } from '@/contexts/modals';
+import { useTomb } from '@/contexts/tomb';
+import { ToastNotifications } from '@/utils/toastNotifications';
+
+export const CreateBucketModal = () => {
+    const { closeModal } = useModal();
+    const { messages } = useIntl();
+    const [newName, setNewName] = useState('');
+    const { } = useTomb();
+
+    const create = async () => {
+        try {
+        } catch (error: any) { };
+    };
+
+    return (
+        <div className="w-modal flex flex-col gap-5" >
+            <div>
+                <h4 className="text-m font-semibold ">{`${messages.createNewBucket}`}</h4>
+            </div>
+            <div>
+                <label>
+                    {`${messages.bucketName}`}
+                    <input
+                        className="mt-2 input w-full h-11 py-3 px-4 rounded-lg border-gray-400 focus:outline-none"
+                        type="text"
+                        placeholder={`${messages.enterNewBucketName}`}
+                        value={newName}
+                        onChange={event => setNewName(event.target.value)}
+                    />
+                </label>
+            </div>
+            <div className="flex items-center gap-3 text-xs" >
+                <button
+                    className="btn-secondary flex-grow py-3 px-4"
+                    onClick={closeModal}
+                >
+                    {`${messages.cancel}`}
+                </button>
+                <button
+                    className="btn-primary flex-grow py-3 px-4"
+                    onClick={create}
+                >
+                    {`${messages.create}`}
+                </button>
+            </div>
+        </div >
+    );
+};
--- crates/banyan-core-service/frontend/src/utils/toastNotifications.tsx
+++ crates/banyan-core-service/frontend/src/utils/toastNotifications.tsx
@@ -1,10 +1,9 @@
 import { ReactElement } from 'react';
-import { toast } from 'react-toastify';
+import { Id, toast } from 'react-toastify';
 
 /** Defines toast notifications with message, toast type and theme. */
 export class ToastNotifications {
-    /** Notifies user.
-    * As default type uses error type, and default theme is colored. */
+
     static notify(
         message: string,
         icon?: ReactElement,
@@ -21,4 +20,26 @@ export class ToastNotifications {
             }
         );
     };
+
+    static promise(pendingMessage: string, successMessage: string, icon: ReactElement, callback: any) {
+        toast.promise(
+            callback,
+            {
+                pending: {
+                    render() { return pendingMessage },
+                },
+                success: {
+                    render() { return successMessage },
+                    icon: <span className="bg-gray-200 p-2 rounded-full">{icon}</span>,
+                },
+            },
+            {
+                theme: 'light',
+                hideProgressBar: true,
+                autoClose: false,
+                bodyStyle: { gap: '17px', fontFamily: 'Inter', fontSize: '14px', fontWeight: 600, color: 'black' },
+                style: { padding: '16px', borderRadius: '12px', borderColor: '#7D89B0', borderWidth: '2px', width: '400px' },
+            }
+        );
+    }
 };
