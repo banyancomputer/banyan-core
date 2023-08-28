ebad96798c6f9cef465bf8b80cd06ae3fd83e204
vladdezhniuk <vladdezhniuk@gmail.com>
2023-08-28 17:30:33 +0300

    feat:loading state added, fallback component added

---
+++ crates/banyan-core-service/frontend/src/components/common/Fallback/index.tsx
@@ -0,0 +1,17 @@
+import React, { ReactElement } from 'react';
+
+import { Loader } from '../Loader';
+
+export const Fallback: React.FC<{
+    children: React.ReactNode;
+    shouldRender: boolean;
+    fallbackComponent?: ReactElement;
+}> = ({
+    children,
+    shouldRender,
+    fallbackComponent = <Loader spinnerSize="50px" containerHeight="200px" />,
+}) => {
+    if (!shouldRender) { return fallbackComponent; }
+
+    return <>{children}</>;
+};
