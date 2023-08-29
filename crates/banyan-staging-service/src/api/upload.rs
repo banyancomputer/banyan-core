f352a09bee16c46574c181c000c5c3d9372c1abd
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2023-08-29 15:51:04 -0400

    feat: further piping of different IO streams together

--- crates/banyan-staging-service/src/api/upload.rs
+++ crates/banyan-staging-service/src/api/upload.rs
@@ -5,8 +5,10 @@ use axum::headers::{ContentLength, ContentType};
 use axum::http::StatusCode;
 use axum::response::{IntoResponse, Response};
 use axum::TypedHeader;
+use futures::{TryFutureExt, TryStream, TryStreamExt};
 use object_store::ObjectStore;
 use serde::{Deserialize, Serialize};
+use tokio::io::{AsyncWrite, AsyncWriteExt};
 use uuid::Uuid;
 
 use crate::database::{BareId, DbError, Executor};
@@ -29,7 +31,6 @@ pub async fn handler(
     body: BodyStream,
 ) -> Result<Response, UploadError> {
     let reported_body_length = content_len.0;
-
     if reported_body_length > client.remaining_storage() {
         return Err(UploadError::InsufficientAuthorizedStorage(
             reported_body_length,
@@ -86,10 +87,16 @@ pub async fn handler(
         }
     };
 
+    //match process_upload_stream(car_field, &mut writer).await {
+    //    Ok(sr) => {
+    //        //writer.shutdown().await.map_err(
+    //    }
+    //}
+
     // handle upload
     //      * if it goes over content length produce a warning
 
-    handle_successful_upload(&db, upload_id, &tmp_file_path).await?;
+    handle_successful_upload(&db, &store, upload_id, &tmp_file_path).await?;
 
     Ok((StatusCode::NO_CONTENT, ()).into_response())
 }
@@ -103,7 +110,7 @@ async fn handle_failed_upload(db: &Database, upload_id: Uuid, _file_path: &str) 
     // todo: should try and clean up the file if it was created
 }
 
-async fn handle_successful_upload(_db: &Database, _upload_id: Uuid, _file_path: &str) -> Result<(), UploadError> {
+async fn handle_successful_upload(_db: &Database, _store: &UploadStore, _upload_id: Uuid, _file_path: &str) -> Result<(), UploadError> {
     // todo: move the file to its final resting place
     // todo: mark upload as successful
     // todo: should enqueue background task to notify the platform
@@ -201,6 +208,37 @@ async fn record_upload_failed(db: &Database, upload_id: Uuid) -> Result<(), Uplo
     Ok(())
 }
 
+struct StoreResult {
+    integrity_hash: String,
+    size: usize,
+}
+
+async fn process_upload_stream<S>(
+    mut stream: S,
+    writer: &mut Box<dyn AsyncWrite + Unpin + Send>,
+) -> Result<StoreResult, UploadStreamError>
+where
+    S: TryStream<Ok = bytes::Bytes, Error = std::io::Error> + Unpin,
+{
+    let mut car_buffer = car_buffer::CarBuffer::new();
+    let mut hasher = blake3::Hasher::new();
+    let mut bytes_written = 0;
+
+    while let Some(chunk) = stream.try_next().await.map_err(UploadStreamError::ReadFailed)? {
+        hasher.update(&chunk);
+        car_buffer.add_chunk(&chunk);
+        bytes_written += chunk.len();
+
+        writer
+            .write_all(&chunk)
+            .await
+            .map_err(UploadStreamError::WriteFailed)?
+    }
+
+    let integrity_hash = hasher.finalize().to_string();
+    Ok(StoreResult { integrity_hash, size: bytes_written })
+}
+
 #[derive(Debug, thiserror::Error)]
 pub enum UploadError {
     #[error("a database error occurred during an upload")]
@@ -224,6 +262,9 @@ pub enum UploadError {
     #[error("we expected a request field but received nothing")]
     RequestFieldMissing,
 
+    #[error("streaming car upload failed")]
+    StreamingFailed(UploadStreamError),
+
     #[error("unable to open store for properly authorized data upload: {0}")]
     StoreUnavailable(object_store::Error),
 }
@@ -232,7 +273,7 @@ impl IntoResponse for UploadError {
     fn into_response(self) -> Response {
         use UploadError::*;
 
-        match &self {
+        match self {
             Database(_) | StoreUnavailable(_) => {
                 tracing::error!("{self}");
                 let err_msg = serde_json::json!({ "msg": "a backend service issue occurred" });
@@ -246,6 +287,122 @@ impl IntoResponse for UploadError {
                 let err_msg = serde_json::json!({ "msg": format!("{self}") });
                 (StatusCode::PAYLOAD_TOO_LARGE, Json(err_msg)).into_response()
             }
+            StreamingFailed(stream_err) => stream_err.into_response(),
+        }
+    }
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum UploadStreamError {
+    #[error("failed to read from client")]
+    ReadFailed(std::io::Error),
+
+    #[error("failed to write to storage backend")]
+    WriteFailed(std::io::Error),
+}
+
+impl IntoResponse for UploadStreamError {
+    fn into_response(self) -> Response {
+        use UploadStreamError::*;
+
+        match self {
+            ReadFailed(_) => {
+                let err_msg = serde_json::json!({ "msg": format!("client stream went away before file upload was complete") });
+                (StatusCode::UNPROCESSABLE_ENTITY, Json(err_msg)).into_response()
+            }
+            WriteFailed(err) => {
+                tracing::error!("writing car file failed: {err}");
+                let err_msg = serde_json::json!({ "msg": "a backend service issue occurred" });
+                (StatusCode::INTERNAL_SERVER_ERROR, Json(err_msg)).into_response()
+            }
+        }
+    }
+}
+
+mod car_buffer {
+    use bytes::Bytes;
+    use futures::TryStreamExt;
+
+    // We only look through the first MiB for the keys section. It's considered badly formed if not
+    // found in that window.
+    const SCANNED_CAR_CAPACITY: usize = 1_048_576;
+
+    enum BufferState {
+        Buffering(Vec<u8>),
+        Done,
+        Empty,
+    }
+
+    pub struct CarBuffer {
+        state: BufferState,
+    }
+
+    impl CarBuffer {
+        pub fn add_chunk(&mut self, bytes: &Bytes) {
+            self.state = match std::mem::replace(&mut self.state, BufferState::Empty) {
+                // We don't need any extra data, just return
+                BufferState::Done => return,
+
+                // This is the first chunk
+                BufferState::Empty => {
+                    let data = bytes
+                        .clone()
+                        .into_iter()
+                        .take(SCANNED_CAR_CAPACITY)
+                        .collect();
+                    BufferState::Buffering(data)
+                }
+                // We've already some data, we just need to extend it to cover as much as possible
+                BufferState::Buffering(mut vec) => {
+                    let remaining_space = SCANNED_CAR_CAPACITY - vec.len();
+                    let consumable_bytes = remaining_space.min(bytes.len());
+
+                    // Add as many bytes as we can to the buffer
+                    vec.extend(bytes.slice(0..consumable_bytes));
+
+                    BufferState::Buffering(vec)
+                }
+            };
+        }
+
+        fn as_slice(&self) -> &[u8] {
+            match &self.state {
+                BufferState::Buffering(vec) => vec.as_slice(),
+                _ => &[],
+            }
+        }
+
+        fn is_done(&self) -> bool {
+            matches!(&self.state, BufferState::Done)
+        }
+
+        pub fn is_ready(&self) -> bool {
+            matches!(&self.state, BufferState::Buffering(_))
+        }
+
+        pub fn new() -> Self {
+            Self {
+                state: BufferState::Empty,
+            }
+        }
+
+        pub fn parse(&mut self) -> Result<Option<()>, &str> {
+            if self.is_done() {
+                return Err("already finished");
+            }
+
+            if !self.is_ready() {
+                return Ok(None);
+            }
+
+            tracing::info!("parsing things out of the car file woo");
+
+            // TODO: process data buffer
+            let _ = self.as_slice();
+            self.state = BufferState::Done;
+
+            // TODO: return something useful
+            Ok(Some(()))
         }
     }
 }
