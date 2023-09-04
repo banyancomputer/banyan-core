ee55faa1bd92d7be0cbd7197ab3a0ace7ceeeb51
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2023-09-04 11:23:49 -0400

    fix: need to use the platform origin and allow post requests

--- crates/banyan-staging-service/src/api/mod.rs
+++ crates/banyan-staging-service/src/api/mod.rs
@@ -12,9 +12,9 @@ use crate::app::State;
 
 pub fn router(state: State) -> Router<State> {
     let cors_layer = CorsLayer::new()
-        .allow_methods(vec![Method::GET])
+        .allow_methods(vec![Method::GET, Method::POST])
         .allow_headers(vec![ACCEPT, ORIGIN])
-        .allow_origin(vec![state.hostname().as_str().parse().unwrap()])
+        .allow_origin(vec![state.platform_base_url().as_str().parse().unwrap()])
         .allow_credentials(true);
 
     Router::new()
