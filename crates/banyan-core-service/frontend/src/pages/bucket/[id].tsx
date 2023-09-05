0252cf9465434171239ede2c3f8269e4e3de3541
vladdezhniuk <vladdezhniuk@gmail.com>
2023-09-06 02:32:23 +0300

    feat: extended logic of single bucket

--- crates/banyan-core-service/frontend/src/pages/bucket/[id].tsx
+++ crates/banyan-core-service/frontend/src/pages/bucket/[id].tsx
@@ -1,19 +1,17 @@
-import React, { useMemo } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { useSearchParams } from 'next/navigation';
-import Link from 'next/link';
-import { useIntl } from 'react-intl';
-import { IoMdAdd } from 'react-icons/io';
-import Image from 'next/image';
 
 import { NextPageWithLayout } from '../page';
 import { useTomb } from '@/contexts/tomb';
-import { useModal } from '@/contexts/modals';
 import getServerSideProps from '@/utils/session';
 
 import BaseLayout from '@/layouts/BaseLayout';
 import { BucketTable } from '@/components/Buckets/BucketTable';
 import { Fallback } from '@/components/common/Fallback';
-import { UploadFileModal } from '@/components/common/Modal/UploadFileModal';
+import { useFolderLocation } from '@/hooks/useFolderLocation';
+import { Bucket } from '@/lib/interfaces/bucket';
+import BucketHeader from '@/components/Bucket/Header';
+
 
 export { getServerSideProps };
 
@@ -21,30 +19,27 @@ const Bucket: NextPageWithLayout = () => {
     const searchParams = useSearchParams();
     const bucketId = searchParams.get('id');
     const { buckets, areBucketsLoading } = useTomb();
-    const { messages } = useIntl();
-    const { openModal, closeModal } = useModal();
-    const selectedBucket = useMemo(() => buckets.find(bucket => bucket.id === bucketId), [buckets, bucketId]);
+    const [selectedBucket, setSelectedBucket] = useState<Bucket>(buckets.find(bucket => bucket.id === bucketId)!);
+    const folderLocation = useFolderLocation();
 
-    const uploadFile = () => {
-        openModal(<UploadFileModal bucket={selectedBucket} />)
-    };
+    useEffect(() => {
+        if (!selectedBucket) return;
+        (async () => {
+            try {
+                const files = await selectedBucket?.mount.ls(folderLocation);
+                setSelectedBucket(bucket => ({ ...bucket, files }));
+            } catch (error: any) { };
+        })()
+    }, [folderLocation])
+
+    useEffect(() => {
+        const bucket = buckets.find(bucket => bucket.id === bucketId)
+        bucket && setSelectedBucket({ ...bucket, files: [...bucket.files] });
+    }, [bucketId, buckets.length])
 
     return (
         <section className="py-9 px-4">
-            <div className="mb-4 flex w-full justify-between items-center">
-                <h2 className="text-xl font-semibold">
-                    <Link href="/">{`${messages.myBuckets}`}</Link>
-                    {' > '}
-                    <Link href={`/bucket/${bucketId}`}>{selectedBucket?.name}</Link>
-                </h2>
-                <button
-                    className="btn-primary gap-2 w-40 py-2 px-4"
-                    onClick={uploadFile}
-                >
-                    <IoMdAdd fill="#fff" size="20px" />
-                    {`${messages.upload}`}
-                </button>
-            </div>
+            <BucketHeader selectedBucket={selectedBucket} />
             <Fallback shouldRender={!areBucketsLoading}>
                 {selectedBucket &&
                     <BucketTable bucket={selectedBucket} />
@@ -54,6 +49,6 @@ const Bucket: NextPageWithLayout = () => {
     );
 };
 
-export default Bucket;
+export default Bucket
 
 Bucket.getLayout = (page) => <BaseLayout>{page}</BaseLayout>;
