3b0230c3c57ca0722a06ed8aac71ab81c7526839
vladdezhniuk <vladdezhniuk@gmail.com>
2023-09-18 15:29:48 +0300

    feat: billing page base layout

---
+++ crates/banyan-core-service/frontend/src/components/Account/Billing/ErrorBanner/index.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+import { IoMdClose } from 'react-icons/io';
+
+export const ErrorBanner: React.FC<{ title: string, description: string }> = ({ description, title }) => {
+    const [isVisible, setIsVisible] = useState(true);
+
+    const toggleStorageVisibility = () => {
+        setIsVisible(prev => !prev);
+    };
+    return (
+        <>
+            {isVisible ?
+                <div className='relative px-4 py-5 rounded-lg bg-errorBanner'>
+                    <button
+                        onClick={toggleStorageVisibility}
+                        className="absolute right-4 top-5"
+                    >
+                        <IoMdClose size="20px" />
+                    </button>
+                    <h4 className='mb-1 text-xs font-semibold'>{title}</h4>
+                    <p className='text-xs'>{description}</p>
+                </div>
+                :
+                null
+            }
+        </>
+    )
+}
