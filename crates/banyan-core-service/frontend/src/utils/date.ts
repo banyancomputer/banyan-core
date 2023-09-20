c7c9fb784eee4b0d0bdbefe7ef518baddc8d0e99
vladdezhniuk <vladdezhniuk@gmail.com>
2023-09-20 23:50:35 +0300

    fix: timestamp error fixed

--- crates/banyan-core-service/frontend/src/utils/date.ts
+++ crates/banyan-core-service/frontend/src/utils/date.ts
@@ -1,6 +1,6 @@
 export const getDateLabel = (timestapm: number) => {
     const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-    const date = new Date(timestapm);
+    const date = new Date(timestapm * 1000);
 
     return `${months[date.getMonth()]} ${date.getDay()}, ${date.getFullYear()}`;
 };
