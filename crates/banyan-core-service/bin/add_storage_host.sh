8091bfca71425037554b4df4d364f690bcb09393
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2023-09-26 11:27:03 -0400

    fix: missing slash in script host (#102)

--- crates/banyan-core-service/bin/add_storage_host.sh
+++ crates/banyan-core-service/bin/add_storage_host.sh
@@ -6,6 +6,6 @@ export STORAGE_HOST_FINGERPRINT=$(cat ../banyan-staging-service/data/platform-au
 cat << EOSQL | sqlite3 ./data/server.db
 DELETE FROM storage_hosts;
 INSERT INTO storage_hosts (name, url, used_storage, available_storage, fingerprint, pem)
-  VALUES ("banyan-staging", "http://127.0.0.1:3002", 0, 549755813888000, "${STORAGE_HOST_FINGERPRINT}", "${STORAGE_HOST_PUBKEY}")
+  VALUES ("banyan-staging", "http://127.0.0.1:3002/", 0, 549755813888000, "${STORAGE_HOST_FINGERPRINT}", "${STORAGE_HOST_PUBKEY}")
   RETURNING id;
 EOSQL
