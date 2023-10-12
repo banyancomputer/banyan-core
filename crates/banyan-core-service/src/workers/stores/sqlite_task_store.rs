0c3eceb5a52d2ebabdf7788f761d270a28da57bd
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2023-10-12 12:24:35 -0400

    feat: minimal enough sqlite worker for now

--- crates/banyan-core-service/src/workers/stores/sqlite_task_store.rs
+++ crates/banyan-core-service/src/workers/stores/sqlite_task_store.rs
@@ -99,8 +99,29 @@ impl TaskStore for SqliteTaskStore {
         // if this query fails or any of our rescheduling fails, we still want to process our task,
         // let these retry again sometime in the future. Ideally we'd randomly shuffle some of
         // these to prevent head-of-line blocking by a single poison task.
-        if let Ok(_task_ids) = pending_retry_tasks {
-            // todo
+        //
+        // Ideally we'd also take one of these ourselves if next_task_id is none but that is some
+        // extra complicated logic we don't need right now
+        if let Ok(task_ids) = pending_retry_tasks {
+            for id in task_ids.into_iter() {
+                // we don't care of these fail either, but we'll stop attempting to retry them once
+                // we hit an error. Something else can handle the trouble
+
+                let state_update_res = sqlx::query!(
+                    r#"UPDATE background_tasks
+                        SET
+                            finished_at = DATETIME('now'),
+                            state = 'timed_out'
+                        WHERE id = $1"#,
+                    id,
+                )
+                .execute(&self.pool)
+                .await;
+
+                if let Err(_) = self.retry(id).await {
+                    break;
+                }
+            }
         }
 
         let chosen_task_id = match next_task_id {
@@ -131,11 +152,24 @@ impl TaskStore for SqliteTaskStore {
         Ok(Some(chosen_task))
     }
 
-    async fn retry(&self, id: String) -> Result<Option<String>, TaskStoreError> {
-        todo!()
+    async fn retry(&self, _id: String) -> Result<Option<String>, TaskStoreError> {
+        // leaving as a todo for now
+        Ok(None)
     }
 
     async fn update_state(&self, id: String, new_state: TaskState) -> Result<(), TaskStoreError> {
-        todo!()
+        // this could probably use some protection against invalid state transitions but I'll leave
+        // that as future work for now.
+        sqlx::query!(
+            r#"UPDATE background_tasks
+                SET state = $1
+                WHERE id = $1;"#,
+            id,
+        )
+        .execute(&self.pool)
+        .await
+        .map_err(|err| TaskStoreError::ConnectionFailure(err.to_string()))?;
+
+        Ok(())
     }
 }
