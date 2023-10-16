de56993a11e91f114d796037e7f41c265f00d2bd
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2023-10-16 15:59:11 -0400

    feat: better safe deletion of bucket keys

--- crates/banyan-core-service/src/api/buckets/keys/mod.rs
+++ crates/banyan-core-service/src/api/buckets/keys/mod.rs
@@ -7,7 +7,7 @@ use axum::Router;
 
 //mod all_bucket_keys;
 //mod create_bucket_key;
-//mod delete_bucket_key;
+mod delete_bucket_key;
 //mod reject_bucket_key;
 mod single_bucket_key;
 
@@ -16,8 +16,7 @@ use crate::app::AppState;
 pub fn router(state: AppState) -> Router<AppState> {
     Router::new()
         //.route("/", get(all_bucket_keys::handler).post(create_bucket_key::handler))
-        .route("/:bucket_key_id", get(single_bucket_key::handler))
-        //.route("/:bucket_key_id", get(single_bucket_key::handler).delete(delete_bucket_key::handler))
+        .route("/:bucket_key_id", get(single_bucket_key::handler).delete(delete_bucket_key::handler))
         //.route("/:bucket_key_id/reject", post(reject_bucket_key::handler))
         .with_state(state)
 }
