b838eaadd3b3386a4ac2b73cd115c67ffb89677c
amiller68 <alexanderscottmiller@gmail.com>
2023-10-30 20:19:25 -0400

    fix: no more allowlist

--- crates/banyan-core-service/frontend/src/lib/db/models/index.ts
+++ crates/banyan-core-service/frontend/src/lib/db/models/index.ts
@@ -1,39 +1,36 @@
 import { Sequelize } from 'sequelize';
-import AllowedEmailModel from './allowedEmail';
 import AccountModel from './account';
 import DeviceApiKeyModel from './deviceApiKey';
 import EscrowedDeviceModel from './escrowedDevice';
 
 
 export const client = new Sequelize({
-    dialect: 'sqlite',
-    storage: process.env.DB_PATH,
-    logging: process.env.NODE_ENV === 'development',
-    sync: { force: false, alter: false },
+	dialect: 'sqlite',
+	storage: process.env.DB_PATH,
+	logging: process.env.NODE_ENV === 'development',
+	sync: { force: false, alter: false },
 });
 
-const AllowedEmail = AllowedEmailModel(client);
 const Account = AccountModel(client);
 const EscrowedDevice = EscrowedDeviceModel(client);
 const DeviceApiKey = DeviceApiKeyModel(client);
 Account.hasOne(EscrowedDevice, {
-    foreignKey: 'account_id', // Snake case
-    sourceKey: 'id', // Snake case
-    onDelete: 'CASCADE',
+	foreignKey: 'account_id', // Snake case
+	sourceKey: 'id', // Snake case
+	onDelete: 'CASCADE',
 });
 Account.hasMany(DeviceApiKey, {
-    foreignKey: 'account_id', // Snake case
-    sourceKey: 'id', // Snake case
-    onDelete: 'CASCADE',
+	foreignKey: 'account_id', // Snake case
+	sourceKey: 'id', // Snake case
+	onDelete: 'CASCADE',
 });
 
 export const models = {
-    AllowedEmail,
-    EscrowedDevice,
-    Account,
-    DeviceApiKey,
+	EscrowedDevice,
+	Account,
+	DeviceApiKey,
 };
 
 export default client;
 export * from './errors';
-export { AllowedEmail, Account, DeviceApiKey, EscrowedDevice };
+export { Account, DeviceApiKey, EscrowedDevice };
