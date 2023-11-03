c72b147c6889492a0aef130e97facb776c821181
amiller68 <alexanderscottmiller@gmail.com>
2023-11-03 17:13:21 -0400

    fix: core serving frontend

--- crates/banyan-core-service/frontend/vite.config.js
+++ crates/banyan-core-service/frontend/vite.config.js
@@ -3,53 +3,61 @@ const zlib = require("zlib");
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import viteCompression from "vite-plugin-compression";
+import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 const PRODUCTION_PLUGINS = [
-  react(),
-  viteCompression({
-    algorithm: "brotliCompress",
-    ext: ".br",
-    compressionOptions: {
-      params: {
-        [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
-      },
-    },
-    threshold: 10240,
-    minRatio: 0.8,
-    deleteOriginalAssets: false,
-  }),
+	react(),
+	viteCompression({
+		algorithm: "brotliCompress",
+		ext: ".br",
+		compressionOptions: {
+			params: {
+				[zlib.constants.BROTLI_PARAM_QUALITY]: 11,
+			},
+		},
+		threshold: 10240,
+		minRatio: 0.8,
+		deleteOriginalAssets: false,
+	}),
+	wasm(),
+	topLevelAwait()
 ];
 
-const DEVELOPMENT_PLUGINS = [react()];
+const DEVELOPMENT_PLUGINS = [
+	react(),
+	wasm(),
+	topLevelAwait()
+];
 
 export default ({ mode }) => {
-  const isProduction = mode === "production";
-  const env = loadEnv(mode, process.cwd(), "");
+	const isProduction = mode === "production";
+	const env = loadEnv(mode, process.cwd(), "");
 
-  return defineConfig({
-    base: "/",
-    define: {
-      "process.env": JSON.stringify(env),
-    },
-    root: path.join(__dirname, "/"),
-    server: {
-      port: 3000,
-    },
-    build: {
-      minify: "esbuild",
-      outDir: path.resolve(__dirname, "dist/"),
-      cssCodeSplit: false,
-    },
-    plugins: isProduction ? PRODUCTION_PLUGINS : DEVELOPMENT_PLUGINS,
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src/"),
-        "@app": path.resolve(__dirname, "./src/app/"),
-        "@components": path.resolve(__dirname, "./src/app/components/"),
-        "@static": path.resolve(__dirname, "./src/app/static/"),
-      },
-    },
-    clean: true,
-    minify: true,
-  });
+	return defineConfig({
+		base: "/",
+		define: {
+			"process.env": JSON.stringify(env),
+		},
+		root: path.join(__dirname, "/"),
+		server: {
+			port: 3000,
+		},
+		build: {
+			minify: "esbuild",
+			outDir: path.resolve(__dirname, "../dist/"),
+			cssCodeSplit: false,
+		},
+		plugins: isProduction ? PRODUCTION_PLUGINS : DEVELOPMENT_PLUGINS,
+		resolve: {
+			alias: {
+				"@": path.resolve(__dirname, "./src/"),
+				"@app": path.resolve(__dirname, "./src/app/"),
+				"@components": path.resolve(__dirname, "./src/app/components/"),
+				"@static": path.resolve(__dirname, "./src/app/static/"),
+			},
+		},
+		clean: true,
+		minify: true,
+	});
 };
