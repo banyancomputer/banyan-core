fef6daf92817e6ac2ef1239416c174c23dbc25b5
vladdezhniuk <vladdezhniuk@gmail.com>
2023-11-15 17:46:13 +0200

    feat: test enviroment setted, utils tests added, "getDateLabel" function error fixed

---
+++ crates/banyan-core-service/frontend/src/app/tests/utils/date.test.ts
@@ -0,0 +1,12 @@
+import { getDateLabel } from "@app/utils/date";
+
+describe(
+    'date',
+    () => {
+        test('getDateLabel', () => {
+            expect(getDateLabel(1700062579)).toBe('Nov 15, 2023');
+            expect(getDateLabel(1700062579, false)).toBe('Nov 15');
+            expect(getDateLabel(0)).toBe('Jan 1, 1970');
+        });
+    }
+);
