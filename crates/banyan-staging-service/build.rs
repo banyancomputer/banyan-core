592cd10c01b3525d52e442d2b8f25d64a32294c1
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2023-11-28 12:10:42 -0500

    fix: weird rebuild always happening in staging

--- crates/banyan-staging-service/build.rs
+++ crates/banyan-staging-service/build.rs
@@ -8,10 +8,6 @@ fn report_build_profile() {
 fn report_enabled_features() {
     let mut enabled_features: Vec<&str> = Vec::new();
 
-    if cfg!(feature = "postgres") {
-        enabled_features.push("postgres");
-    }
-
     if cfg!(feature = "sqlite") {
         enabled_features.push("sqlite");
     }
@@ -46,11 +42,6 @@ fn main() {
     // When this script changes, the changes will likely affect the produced binary (or why is it
     // being done here)? Not sure why this isn't the default but here it is...
     println!("cargo:rerun-if-changed=build.rs");
-
-    // Because we're including tags, commit IDs, and dirty status we want to build a new binary
-    // with the correct information when we make a commit or tag a commit.
-    println!("cargo:rerun-if-changed=.git/refs/heads");
-    println!("cargo:rerun-if-changed=.git/refs/tags");
 
     // Migrations lie outside of our normal source but are both embedded in our code, and our code
     // is dependent on the changes they represent. We should be building when this changes
