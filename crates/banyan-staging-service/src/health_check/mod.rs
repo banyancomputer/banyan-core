bdad7b734490a5addbccd808eb10c0565c42ae05
Al Miller <alexanderscottmiller@gmail.com>
2023-12-05 15:40:07 -0500

    fix: no jobs route

--- crates/banyan-staging-service/src/health_check/mod.rs
+++ crates/banyan-staging-service/src/health_check/mod.rs
@@ -6,7 +6,6 @@ use tower_http::cors::{Any, CorsLayer};
 use tower_http::limit::RequestBodyLimitLayer;
 
 mod data_source;
-mod jobs;
 mod liveness;
 mod readiness;
 mod version;
@@ -27,7 +26,6 @@ pub fn router(state: AppState) -> Router<AppState> {
     Router::new()
         .route("/healthz", get(liveness::handler))
         .route("/readyz", get(readiness::handler))
-        .route("/jobz", get(jobs::handler))
         .route("/version", get(version::handler))
         .layer(cors_layer)
         .layer(RequestBodyLimitLayer::new(HEALTHCHECK_REQUEST_SIZE_LIMIT))
