a60517c58c387555410dba059cc47d9eab636033
vladdezhniuk <vladdezhniuk@gmail.com>
2023-12-08 23:58:08 +0200

    feat: code commented

--- crates/banyan-core-service/frontend/src/app/contexts/tomb.tsx
+++ crates/banyan-core-service/frontend/src/app/contexts/tomb.tsx
@@ -395,24 +395,24 @@ export const TombProvider = ({ children }: { children: ReactNode }) => {
 		const termsClient = new TermsAndColditionsClient();
 		(async () => {
 			try {
-				const termsAndConditions = await termsClient.getTermsAndCondition();
-				const userData = await userClient.getCurrentUser();
+				// const termsAndConditions = await termsClient.getTermsAndCondition();
+				// const userData = await userClient.getCurrentUser();
 
-				if (!userData) return;
+				// if (!userData) return;
 
-				if (!userData.accepted_tos_at || userData.accepted_tos_at <= +termsAndConditions.tos_date) {
-					/** TODO: uncoment when company name endpoint would be merged. */
-					// openModal(
-					// 	<TermsAndConditionsModal
-					// 		setAreTermsAccepted={setAreTermsAccepted}
-					// 		terms={termsAndConditions.tos_content}
-					// 		userData={userData} />
-					// 	, null, true);
+				// if (!userData.accepted_tos_at || userData.accepted_tos_at <= +termsAndConditions.tos_date) {
+				// 	/** TODO: uncoment when company name endpoint would be merged. */
+				// 	openModal(
+				// 		<TermsAndConditionsModal
+				// 			setAreTermsAccepted={setAreTermsAccepted}
+				// 			terms={termsAndConditions.tos_content}
+				// 			userData={userData} />
+				// 		, null, true);
 
-					return;
-				}
+				// 	return;
+				// }
 
-				setAreTermsAccepted(true);
+				// setAreTermsAccepted(true);
 			} catch (error: any) {
 				console.log(error);
 			}
