75282db96d131afd51f84d3d8ff8be3d14acfb05
Al Miller <alexanderscottmiller@gmail.com>
2023-12-12 17:42:57 -0500

    fix: after merge

--- crates/banyan-core-service/src/api/share/mod.rs
+++ crates/banyan-core-service/src/api/share/mod.rs
@@ -1,11 +1,20 @@
 use axum::routing::get;
-use axum::Router;
+use std::error::Error;
 
-use crate::app::AppState;
+use axum::body::HttpBody;
+use axum::Router;
 
 mod shared_file;
 
-pub fn router(state: AppState) -> Router<AppState> {
+use crate::app::AppState;
+
+pub fn router<B>(state: AppState) -> Router<AppState, B>
+where
+    B: HttpBody + Send + 'static,
+    B::Data: Send + 'static,
+    Box<dyn Error + Send + Sync + 'static>: From<B::Error>,
+    bytes::Bytes: From<<B as HttpBody>::Data>,
+{
     Router::new()
         .route("/", get(shared_file::handler))
         .with_state(state)
