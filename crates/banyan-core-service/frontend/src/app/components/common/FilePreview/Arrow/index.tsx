375d3437a01d2d0f2f72962fda43f42ffaf83633
vladdezhniuk <vladdezhniuk@gmail.com>
2023-12-12 18:03:46 +0200

    feat: extendedfile preview logic

---
+++ crates/banyan-core-service/frontend/src/app/components/common/FilePreview/Arrow/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { ChevronUp } from '@static/images/common';
+
+export const PreviewArrow: React.FC<{ action: () => void, isVisivle: boolean, className?: string }> = ({ action, isVisivle, className }) => {
+    return (
+        <>
+            {isVisivle ?
+                <button
+                    onClick={action}
+                    className={`fixed top-1/2 -translate-y-1/2 p-4 rounded-full bg-black text-white z-40 transition-all hover:bg-gray-800 ${className}`}
+                >
+                    <ChevronUp width="40px" height="40px" />
+                </button>
+                : null
+            }
+        </>
+    )
+}
