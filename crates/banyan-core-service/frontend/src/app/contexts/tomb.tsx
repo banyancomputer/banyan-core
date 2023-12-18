001c9cd793f442c037705bea403c986445f656e6
vladdezhniuk <vladdezhniuk@gmail.com>
2023-12-18 15:38:37 +0200

    feat: bucket creation logic change

--- crates/banyan-core-service/frontend/src/app/contexts/tomb.tsx
+++ crates/banyan-core-service/frontend/src/app/contexts/tomb.tsx
@@ -179,27 +179,22 @@ export const TombProvider = ({ children }: { children: ReactNode }) => {
 	const createBucketAndMount = async (name: string, storageClass: string, bucketType: string): Promise<string> => {
 		return await tombMutex(tomb, async tomb => {
 			const key = await getEncryptionKey();
-			const wasmBucketMount = await tomb!.createBucketAndMount(name, storageClass, bucketType, key.privatePem, key.publicPem);
-			const wasmBucket = wasmBucketMount.bucket;
-			const wasmMount = wasmBucketMount.mount;
-			const files = await wasmMount.ls([]);
-			const snapshots = await tomb!.listBucketSnapshots(wasmBucket.id());
-			const locked = await wasmMount.locked();
-			const isSnapshotValid = await wasmMount.hasSnapshot();
+			const { bucket: wasmBucket, mount: wasmMount } = await tomb!.createBucketAndMount(name, storageClass, bucketType, key.privatePem, key.publicPem);
 			const bucket = {
 				mount: wasmMount,
 				id: wasmBucket.id(),
 				name: wasmBucket.name(),
 				storageClass: wasmBucket.storageClass(),
 				bucketType: wasmBucket.bucketType(),
-				files: files || [],
-				snapshots,
+				files: [],
+				snapshots: [],
 				keys: [],
-				locked,
-				isSnapshotValid
+				locked: false,
+				isSnapshotValid: false
 			};
+
 			setBuckets(prev => [...prev, bucket].sort((a, b) => a.name.localeCompare(b.name)));
-			return bucket.id;
+			return bucket.id
 		});
 	};
 
