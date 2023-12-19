713f02299aafe4e6c347d936be8dc93aa96582c1
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2023-12-19 16:52:51 -0500

    feat: more tests, fix pending recording

--- crates/banyan-core-service/src/database/models/pending_expiration.rs
+++ crates/banyan-core-service/src/database/models/pending_expiration.rs
@@ -19,9 +19,10 @@ impl PendingExpiration {
         let mut block_id_builder = sqlx::QueryBuilder::new(
             r#"SELECT b.id FROM blocks AS b
                   JOIN block_locations AS bl ON bl.block_id = b.id
-                  WHERE bl.metadata_id = $1
-                      AND b.cid IN "#,
+                  WHERE bl.metadata_id = "#,
         );
+        block_id_builder.push_bind(metadata_id);
+        block_id_builder.push(" AND b.cid IN (");
 
         while let Some(cid) = block_cid_iterator.next() {
             block_id_builder.push_bind(cid);
@@ -31,7 +32,8 @@ impl PendingExpiration {
             }
         }
 
-        block_id_builder.push(";");
+        block_id_builder.push(");");
+
         let block_ids: Vec<String> = block_id_builder
             .build_query_scalar()
             .persistent(false)
@@ -44,16 +46,16 @@ impl PendingExpiration {
             return Ok(());
         }
 
-        let mut pending_association_query = sqlx::QueryBuilder::new(
-            "INSERT INTO pending_expirations (metadata_id, block_id) VALUES ",
-        );
+        let mut pending_association_query =
+            sqlx::QueryBuilder::new("INSERT INTO pending_expirations (metadata_id, block_id) ");
 
-        pending_association_query.push_tuples(block_ids, |mut paq, bid| {
+        pending_association_query.push_values(block_ids, |mut paq, bid| {
             paq.push_bind(metadata_id);
             paq.push_bind(bid);
         });
 
         pending_association_query.push(";");
+
         pending_association_query
             .build()
             .execute(&mut *conn)
@@ -66,19 +68,9 @@ impl PendingExpiration {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    use crate::database::models::MetadataState;
     use crate::database::test_helpers::*;
-
-    #[tokio::test]
-    #[ignore]
-    async fn test_empty_block_associations() {
-        todo!()
-    }
-
-    #[tokio::test]
-    #[ignore]
-    async fn test_unknown_block_associations() {
-        todo!()
-    }
 
     #[tokio::test]
     async fn test_pending_expiration_block_association() {
@@ -87,10 +79,42 @@ mod tests {
 
         let user_id = sample_user(&mut conn, "user@domain.tld").await;
         let bucket_id = sample_bucket(&mut conn, &user_id).await;
+        let metadata_id = sample_metadata(&mut conn, &bucket_id, 1, MetadataState::Current).await;
         let storage_host_id = create_storage_host(&mut conn, "SP", "https://[::1]:8001/", 0).await;
 
         let blk_cids: Vec<_> = normalize_cids(generate_cids(data_generator(0..3))).collect();
+        let mut blk_ids = create_blocks(&mut conn, blk_cids.iter().map(String::as_str)).await;
 
-        todo!()
+        // make comparison consistent later on
+        blk_ids.sort();
+
+        associate_blocks(
+            &mut conn,
+            &metadata_id,
+            &storage_host_id,
+            blk_ids.iter().map(String::as_str),
+        )
+        .await;
+
+        PendingExpiration::record_pending_block_expirations(
+            &mut conn,
+            &metadata_id,
+            blk_cids.iter().map(String::as_str),
+        )
+        .await
+        .expect("recording success");
+
+        let mut pending_block_list = sqlx::query_scalar!(
+            "SELECT block_id FROM pending_expirations WHERE metadata_id = $1;",
+            metadata_id
+        )
+        .fetch_all(&mut *conn)
+        .await
+        .expect("retrieving pending expirations");
+
+        // more consistency
+        pending_block_list.sort();
+
+        assert_eq!(pending_block_list, blk_ids);
     }
 }
