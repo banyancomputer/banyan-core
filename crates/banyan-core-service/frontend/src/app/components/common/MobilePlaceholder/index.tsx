40947fa58bf61aea34a252467976b76f0a632b03
Vlad Dezhniuk <77901473+vladdezhniuk@users.noreply.github.com>
2023-12-27 07:13:36 -0800

    feat: added mobile placeholder page, remover possibility to delete last bucket (#295)

---
+++ crates/banyan-core-service/frontend/src/app/components/common/MobilePlaceholder/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useIntl } from 'react-intl';
+
+import { Folders, Logo } from '@static/images/common'
+
+export const MobilePlaceholder = () => {
+    const { messages } = useIntl();
+
+    return (
+        <section className="hidden w-screen h-screen flex-col items-stretch text-[#57221E] max-sm:flex">
+            <div className="flex justify-center items-center px-16 py-8 bg-termsAndConditions-header">
+                <Logo width="195px" height="40px" />
+            </div>
+            <div className="flex-grow flex flex-col items-center justify-center bg-white">
+                <Folders />
+                <div className="mt-6 flex flex-col items-center text-text-900">
+                    <h4 className="mb-3 font-semibold text-[24px]">{`${messages.youAreReadyForStart}`}</h4>
+                    <p>{`${messages.useDesktop}`}</p>
+                </div>
+            </div>
+        </section>
+    )
+}
