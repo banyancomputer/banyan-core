e24c569abbade8b32d8b6d5e65d7b0e3747a713a
Al Miller <alexanderscottmiller@gmail.com>
2024-01-02 10:35:33 -0500

    Alex/eng 468 improved timestamp precision in sqlx (#297)
    * feat: database timestamp
    
    * fix: precision timestamp database type
    
    * fix: leftover env
    
    * fix: metadata using precise timestamps
    
    * feat: test that defaults are interpretable
    
    * feat: more test that sqlite timestamps are interpretable
    
    * fix: ordering tests
    
    * fix: ci
    
    ---------
    
    Co-authored-by: Sam Stelfox <sstelfox@bedroomprogrammers.net>

--- crates/banyan-core-service/src/database/models/metadata.rs
+++ crates/banyan-core-service/src/database/models/metadata.rs
@@ -1,3 +1,5 @@
+use time::OffsetDateTime;
+
 use crate::database::models::Bucket;
 use crate::database::DatabaseConnection;
 
@@ -18,14 +20,18 @@ impl NewMetadata<'_> {
     /// returns the ID of the newly created row. By default this initialized the state of the
     /// metadata row to 'uploading', which is the entry-point into the metadata state machine.
     pub async fn save(&self, conn: &mut DatabaseConnection) -> Result<String, sqlx::Error> {
+        // Note: default timestamp values are not sufficient, so force the value to be set to a precise form
+        // by including the timestamp in the query.
+        let now = OffsetDateTime::now_utc();
         sqlx::query_scalar!(
-            r#"INSERT INTO metadata (bucket_id, metadata_cid, root_cid, expected_data_size, state)
-                   VALUES ($1, $2, $3, $4, 'uploading')
+            r#"INSERT INTO metadata (bucket_id, metadata_cid, root_cid, expected_data_size, state, created_at, updated_at)
+                   VALUES ($1, $2, $3, $4, 'uploading', $5, $5)
                    RETURNING id;"#,
             self.bucket_id,
             self.metadata_cid,
             self.root_cid,
             self.expected_data_size,
+            now,
         )
         .fetch_one(&mut *conn)
         .await
@@ -55,14 +61,18 @@ impl Metadata {
         metadata_id: &str,
         data_size: Option<i64>,
     ) -> Result<(), sqlx::Error> {
+        // Note: default timestamp values are not sufficient, so force the value to be set to a precise form
+        // by including the timestamp in the query.
+        let now = OffsetDateTime::now_utc();
         let current_result = sqlx::query!(
-            "UPDATE metadata SET state = 'current', data_size = $3
+            "UPDATE metadata SET state = 'current', data_size = $3, updated_at = $4
                  WHERE bucket_id = $1
                      AND id = $2
                      AND state IN ('pending', 'uploading');",
             bucket_id,
             metadata_id,
             data_size,
+            now,
         )
         .execute(&mut *conn)
         .await?;
@@ -81,12 +91,13 @@ impl Metadata {
         }
 
         let result = sqlx::query!(
-            r#"UPDATE metadata SET state = 'outdated'
+            r#"UPDATE metadata SET state = 'outdated', updated_at = $3
                    WHERE bucket_id = $1
                        AND id != $2
                        AND state = 'current';"#,
             bucket_id,
             metadata_id,
+            now,
         )
         .execute(&mut *conn)
         .await?;
@@ -124,15 +135,20 @@ impl Metadata {
         metadata_hash: &str,
         metadata_size: i64,
     ) -> Result<(), sqlx::Error> {
+        // Note: default timestamp values are not sufficient, so force the value to be set to a precise form
+        // by including the timestamp in the query.
+        let now = OffsetDateTime::now_utc();
         sqlx::query!(
             r#"UPDATE metadata
                    SET metadata_hash = $2,
                        metadata_size = $3,
-                       state = 'pending'
+                       state = 'pending',
+                       updated_at = $4
                    WHERE id = $1;"#,
             metadata_id,
             metadata_hash,
             metadata_size,
+            now
         )
         .execute(&mut *conn)
         .await?;
@@ -143,6 +159,8 @@ impl Metadata {
 
 #[cfg(test)]
 mod tests {
+    use time::macros::datetime;
+
     use super::*;
     use crate::database::models::MetadataState;
     use crate::database::test_helpers::*;
@@ -187,5 +205,394 @@ mod tests {
 
         let result = Metadata::mark_current(&mut conn, &bucket_id, "fake-id", None).await;
         assert!(result.is_err());
+    }
+
+    // TODO: it would be ideal if we could test whether our structs implements the
+    // correct use of timestamps, but that's not tenable since we can't control the
+    // the timestamp values that they initialize rows with (those are side effects
+    // of methods like NewMetadata::save(), Metadata::mark_current(),
+    // Metadata::upload_complete()). These are proximate tests that ensure that, at the
+    // very least, the timestamps are being set to the correct values by SQLX, and that
+    // everything is okay so long as we manually set the timestamp values and don't rely
+    // on the defaults. Even then, we test whether defaults are sufficient for
+    // timestamps to be deserialized correctly when reading from the database (in other
+    // words though defaults have a different format and no precision, they are still
+    // interpretable as OffsetDateTime).
+
+    #[tokio::test]
+    async fn test_metadata_timestamps_have_precision() {
+        let db = setup_database().await;
+        let mut conn = db.begin().await.expect("connection");
+
+        let user_id = sample_user(&mut conn, "user@domain.tld").await;
+        let bucket_id = sample_bucket(&mut conn, &user_id).await;
+
+        let timestamp = datetime!(2021-01-01 00:00:00.000_000_001 UTC);
+        let metadata_id = create_metadata(
+            &mut conn,
+            &bucket_id,
+            "root-cid",
+            "metadata-cid",
+            MetadataState::Current,
+            Some(timestamp),
+        )
+        .await;
+
+        let (created_at, updated_at) = metadata_timestamps(&mut conn, &metadata_id).await;
+
+        // Assert that the timestamps have utmost precision of nanoseconds
+        assert_eq!(created_at, timestamp);
+        assert_eq!(updated_at, timestamp);
+        assert_eq!(created_at.nanosecond(), 1);
+        assert_eq!(updated_at.nanosecond(), 1);
+    }
+
+    #[tokio::test]
+    async fn test_metadata_timestamps_have_no_hour_truncation() {
+        let db = setup_database().await;
+        let mut conn = db.begin().await.expect("connection");
+
+        let user_id = sample_user(&mut conn, "user@domain.tld").await;
+        let bucket_id = sample_bucket(&mut conn, &user_id).await;
+
+        let timestamp = datetime!(2021-01-01 09:59:59.999_999_999 UTC);
+        let metadata_id = create_metadata(
+            &mut conn,
+            &bucket_id,
+            "root-cid",
+            "metadata-cid",
+            MetadataState::Current,
+            Some(timestamp),
+        )
+        .await;
+
+        let (created_at, updated_at) = metadata_timestamps(&mut conn, &metadata_id).await;
+
+        // Assert that the timestamps have utmost precision of nanoseconds
+        assert_eq!(created_at, timestamp);
+        assert_eq!(updated_at, timestamp);
+        assert_eq!(created_at.nanosecond(), 999_999_999);
+
+        let (raw_created_at, raw_updated_at) =
+            raw_metadata_timestamps(&mut conn, &metadata_id).await;
+
+        // Split the text at 'T' and assert the character following the 'T' is a '0'
+        assert_eq!(
+            raw_created_at
+                .split('T')
+                .nth(1)
+                .unwrap()
+                .chars()
+                .next()
+                .unwrap(),
+            '0'
+        );
+        assert_eq!(
+            raw_updated_at
+                .split('T')
+                .nth(1)
+                .unwrap()
+                .chars()
+                .next()
+                .unwrap(),
+            '0'
+        );
+    }
+
+    #[tokio::test]
+    async fn test_metadata_timestamps_have_subsecond_truncation() {
+        let db = setup_database().await;
+        let mut conn = db.begin().await.expect("connection");
+
+        let user_id = sample_user(&mut conn, "user@domain.tld").await;
+        let bucket_id = sample_bucket(&mut conn, &user_id).await;
+
+        let timestamp = datetime!(2021-01-01 09:59:59.999 UTC);
+        let metadata_id = create_metadata(
+            &mut conn,
+            &bucket_id,
+            "root-cid",
+            "metadata-cid",
+            MetadataState::Current,
+            Some(timestamp),
+        )
+        .await;
+
+        let (created_at, updated_at) = metadata_timestamps(&mut conn, &metadata_id).await;
+
+        // Assert that the timestamps have utmost precision of nanoseconds
+        assert_eq!(created_at, timestamp);
+        assert_eq!(updated_at, timestamp);
+        assert_eq!(created_at.nanosecond(), 999_000_000);
+
+        let (raw_created_at, raw_updated_at) =
+            raw_metadata_timestamps(&mut conn, &metadata_id).await;
+
+        // Get the Text after '.' and before 'Z' and assert that it is '999'
+        assert_eq!(
+            raw_created_at
+                .split('.')
+                .nth(1)
+                .unwrap()
+                .split('Z')
+                .next()
+                .unwrap(),
+            "999"
+        );
+        assert_eq!(
+            raw_updated_at
+                .split('.')
+                .nth(1)
+                .unwrap()
+                .split('Z')
+                .next()
+                .unwrap(),
+            "999"
+        );
+    }
+
+    #[tokio::test]
+    async fn test_default_sqlx_timestamps_have_no_precision_and_are_interpretable() {
+        let db = setup_database().await;
+        let mut conn = db.begin().await.expect("connection");
+
+        let user_id = sample_user(&mut conn, "user@domain.tld").await;
+        let bucket_id = sample_bucket(&mut conn, &user_id).await;
+
+        // Create a metadata row with no timestamp values -- this should use the defaults
+        let root_cid = "root-cid";
+        let metadata_cid = "metadata-cid";
+        let state = MetadataState::Current;
+        let metadata_id = sqlx::query_scalar!(
+            r#"INSERT INTO
+                    metadata (bucket_id, root_cid, metadata_cid, expected_data_size, state)
+                    VALUES ($1, $2, $3, 0, $4)
+                    RETURNING id;"#,
+            bucket_id,
+            root_cid,
+            metadata_cid,
+            state
+        )
+        .fetch_one(&mut *conn)
+        .await
+        .expect("inserting metadata");
+
+        // See if we can read these as OffsetDateTime
+        let (created_at, updated_at) = metadata_timestamps(&mut conn, &metadata_id).await;
+
+        // Assert that the timestamps have no nanosecond precision
+        assert_eq!(created_at.nanosecond(), 0);
+        assert_eq!(updated_at.nanosecond(), 0);
+
+        let (raw_created_at, raw_updated_at) =
+            raw_metadata_timestamps(&mut conn, &metadata_id).await;
+
+        // Do simple formatting checks on the raw timestamps using the length of the string
+        assert_eq!(raw_created_at.len(), 19);
+        assert_eq!(raw_updated_at.len(), 19);
+
+        // Split the text at ' ' and assert that each part has the correct length
+        let mut raw_iterable_created_at = raw_created_at.split(' ');
+        let mut raw_iterable_updated_at = raw_updated_at.split(' ');
+
+        assert_eq!(raw_iterable_created_at.next().unwrap().len(), 10);
+        assert_eq!(raw_iterable_updated_at.next().unwrap().len(), 10);
+        assert_eq!(raw_iterable_created_at.next().unwrap().len(), 8);
+        assert_eq!(raw_iterable_updated_at.next().unwrap().len(), 8);
+    }
+
+    #[tokio::test]
+    async fn test_datetime_now_sqlx_timestamps_have_no_precision_and_are_interpretable() {
+        let db = setup_database().await;
+        let mut conn = db.begin().await.expect("connection");
+
+        let user_id = sample_user(&mut conn, "user@domain.tld").await;
+        let bucket_id = sample_bucket(&mut conn, &user_id).await;
+
+        let metadata_id = create_metadata(
+            &mut conn,
+            &bucket_id,
+            "root-cid",
+            "metadata-cid",
+            MetadataState::Current,
+            None,
+        )
+        .await;
+
+        // Set the timestamps to sqlite values of datetime('now')
+        sqlx::query!(
+            "UPDATE metadata SET created_at = DATETIME('now'), updated_at = DATETIME('now') WHERE id = $1;",
+            metadata_id
+        )
+        .execute(&mut *conn)
+        .await
+        .expect("updating timestamps");
+
+        // See if we can read these as OffsetDateTime
+        let (created_at, updated_at) = metadata_timestamps(&mut conn, &metadata_id).await;
+
+        // Assert that the timestamps have no nanosecond precision
+        assert_eq!(created_at.nanosecond(), 0);
+        assert_eq!(updated_at.nanosecond(), 0);
+
+        let (raw_created_at, raw_updated_at) =
+            raw_metadata_timestamps(&mut conn, &metadata_id).await;
+
+        // Do simple formatting checks on the raw timestamps using the length of the string
+        assert_eq!(raw_created_at.len(), 19);
+        assert_eq!(raw_updated_at.len(), 19);
+
+        // Split the text at ' ' and assert that each part has the correct length
+        let mut raw_iterable_created_at = raw_created_at.split(' ');
+        let mut raw_iterable_updated_at = raw_updated_at.split(' ');
+
+        assert_eq!(raw_iterable_created_at.next().unwrap().len(), 10);
+        assert_eq!(raw_iterable_updated_at.next().unwrap().len(), 10);
+        assert_eq!(raw_iterable_created_at.next().unwrap().len(), 8);
+        assert_eq!(raw_iterable_updated_at.next().unwrap().len(), 8);
+    }
+
+    #[tokio::test]
+    async fn test_sqlx_and_precise_timestamps_are_orderable() {
+        let db = setup_database().await;
+        let mut conn = db.begin().await.expect("connection");
+
+        // Test case where the sqlite timestamp is before the precise timestamp
+
+        let user_id = sample_user(&mut conn, "user-1@domain.tld").await;
+
+        let bucket_id = sample_bucket(&mut conn, &user_id).await;
+
+        // Create a metadata row with no timestamp values -- this should use the defaults
+        let root_cid = "root-cid";
+        let metadata_cid = "metadata-cid";
+        let state = MetadataState::Current;
+        let first_metadata_id = sqlx::query_scalar!(
+            r#"INSERT INTO
+                    metadata (bucket_id, root_cid, metadata_cid, expected_data_size, state)
+                    VALUES ($1, $2, $3, 0, $4)
+                    RETURNING id;"#,
+            bucket_id,
+            root_cid,
+            metadata_cid,
+            state
+        )
+        .fetch_one(&mut *conn)
+        .await
+        .expect("inserting metadata");
+
+        // Create a second precisely timestamped metadata row a little bit later
+        let timestamp = OffsetDateTime::now_utc() + time::Duration::hours(1);
+        let second_metadata_id = create_metadata(
+            &mut conn,
+            &bucket_id,
+            "root-cid",
+            "metadata-cid",
+            MetadataState::Current,
+            Some(timestamp),
+        )
+        .await;
+
+        // Query all metadata ids in order of creation
+        let metadata_ids = sqlx::query_scalar!("SELECT id FROM metadata ORDER BY created_at ASC;")
+            .fetch_all(&mut *conn)
+            .await
+            .expect("querying metadata ids");
+
+        // Assert that the first metadata id is the first one created
+        assert_eq!(metadata_ids[0], first_metadata_id);
+        assert_eq!(metadata_ids[1], second_metadata_id);
+
+        // Query all metadata ids in reverse order of creation
+        let metadata_ids = sqlx::query_scalar!("SELECT id FROM metadata ORDER BY created_at DESC;")
+            .fetch_all(&mut *conn)
+            .await
+            .expect("querying metadata ids");
+
+        // Assert that the first metadata id is the first one created
+        assert_eq!(metadata_ids[0], second_metadata_id);
+        assert_eq!(metadata_ids[1], first_metadata_id);
+
+        // Get the OffsetDateTime values for the first and second metadata ids
+        let (first_created_at, first_updated_at) =
+            metadata_timestamps(&mut conn, &first_metadata_id).await;
+        let (second_created_at, second_updated_at) =
+            metadata_timestamps(&mut conn, &second_metadata_id).await;
+
+        // Assert that the first metadata id is the first one created
+        assert!(first_created_at < second_created_at);
+        assert!(first_updated_at < second_updated_at);
+
+        // Test case where the sqlite timestamp is after the precise timestamp
+
+        let user_id = sample_user(&mut conn, "user-2@domain.tld").await;
+
+        let bucket_id = sample_bucket(&mut conn, &user_id).await;
+
+        // Create a piece of metadata with a precise timestamp far in the past
+        let timestamp = datetime!(1970-01-01 00:00:00 UTC);
+        let first_metadata_id = create_metadata(
+            &mut conn,
+            &bucket_id,
+            "root-cid",
+            "metadata-cid",
+            MetadataState::Current,
+            Some(timestamp),
+        )
+        .await;
+
+        // Create a second metadata row with no timestamp values -- this should use the defaults
+        let root_cid = "root-cid";
+        let metadata_cid = "metadata-cid";
+        let state = MetadataState::Current;
+        let second_metadata_id = sqlx::query_scalar!(
+            r#"INSERT INTO
+                    metadata (bucket_id, root_cid, metadata_cid, expected_data_size, state)
+                    VALUES ($1, $2, $3, 0, $4)
+                    RETURNING id;"#,
+            bucket_id,
+            root_cid,
+            metadata_cid,
+            state
+        )
+        .fetch_one(&mut *conn)
+        .await
+        .expect("inserting metadata");
+
+        // Query all metadata ids in order of creation
+        let metadata_ids = sqlx::query_scalar!(
+            "SELECT id FROM metadata WHERE bucket_id = $1 ORDER BY created_at ASC;",
+            bucket_id
+        )
+        .fetch_all(&mut *conn)
+        .await
+        .expect("querying metadata ids");
+
+        // Assert that the first metadata id is the first one created
+        assert_eq!(metadata_ids[0], first_metadata_id);
+        assert_eq!(metadata_ids[1], second_metadata_id);
+
+        // Query all metadata ids in reverse order of creation
+        let metadata_ids = sqlx::query_scalar!(
+            "SELECT id FROM metadata WHERE bucket_id = $1 ORDER BY created_at DESC;",
+            bucket_id
+        )
+        .fetch_all(&mut *conn)
+        .await
+        .expect("querying metadata ids");
+
+        // Assert that the first metadata id is the first one created
+        assert_eq!(metadata_ids[0], second_metadata_id);
+        assert_eq!(metadata_ids[1], first_metadata_id);
+
+        // Get the OffsetDateTime values for the first and second metadata ids
+        let (first_created_at, first_updated_at) =
+            metadata_timestamps(&mut conn, &first_metadata_id).await;
+        let (second_created_at, second_updated_at) =
+            metadata_timestamps(&mut conn, &second_metadata_id).await;
+
+        // Assert that the first metadata id is the first one created
+        assert!(first_created_at < second_created_at);
+        assert!(first_updated_at < second_updated_at);
     }
 }
