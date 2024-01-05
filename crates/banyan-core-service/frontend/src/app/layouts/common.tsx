a60013f9d54f3b13e799eb7acc2f1e70ee1f5ce4
vladdezhniuk <vladdezhniuk@gmail.com>
2024-01-05 20:06:09 +0200

    feat: beta banner uncommented

--- crates/banyan-core-service/frontend/src/app/layouts/common.tsx
+++ crates/banyan-core-service/frontend/src/app/layouts/common.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { ErrorBanner } from '@components/common/ErrorBanner';
 import { Header } from '@components/common/Header';
 import { Navigation } from '@components/common/Navigation';
+import { BetaBanner } from '../components/common/BetaBanner';
 
 export const CommonLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
@@ -10,7 +11,7 @@ export const CommonLayout: React.FC<{ children: React.ReactNode }> = ({ children
       <Navigation />
       <section className="flex-grow flex flex-col h-screen overflow-y-scroll">
         <Header />
-        {/* <BetaBanner /> */}
+        <BetaBanner />
         <ErrorBanner />
         {children}
       </section>
