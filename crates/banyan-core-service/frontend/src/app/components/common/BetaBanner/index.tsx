d66e61416da880e0287db12e3ee0efa4b07b9300
vladdezhniuk <vladdezhniuk@gmail.com>
2024-01-09 15:27:04 +0200

    feat: error banner redesigned, beta banner redesigned

--- crates/banyan-core-service/frontend/src/app/components/common/BetaBanner/index.tsx
+++ crates/banyan-core-service/frontend/src/app/components/common/BetaBanner/index.tsx
@@ -1,13 +1,10 @@
 import { useIntl } from 'react-intl';
 
-import { ErrorBannerIcon } from '@static/images/common';
-
 export const BetaBanner = () => {
     const { messages } = useIntl();
 
     return (
-        <div className="mb-2 flex justify-center items-center gap-2 py-4 px-2 bg-errorBanner border-2 border-errorBannerBorder text-[10px] leading-3 tracking-tighter font-medium text-text-900">
-            <ErrorBannerIcon width="14px" height="14px" />
+        <div className="flex items-center gap-2 p-4 bg-betaBanner border-2 border-betaBannerBorder text-xs leading-3 tracking-tighter font-semibold text-text-900">
             {`${messages.betaBannerWarning}`}
         </div>
     );
