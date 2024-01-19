581e9b7bd7d70d0f973203ad200c21ead755d57c
Plamen Hristov <phristov@blocksoft.ltd>
2024-01-19 22:31:41 +0200

    ENG-505: Recurrring background task (#320)
    https://linear.app/banyan/issue/ENG-505/add-support-for-recurring-tasks-to-background-task-system

--- crates/banyan-task/src/lib.rs
+++ crates/banyan-task/src/lib.rs
@@ -2,35 +2,36 @@
 
 use std::time::Duration;
 
-mod current_task;
+mod models;
 pub mod panic_safe_future;
 mod queue_config;
 mod stores;
-mod task;
 mod task_instance_builder;
 mod task_like;
-mod task_state;
 mod task_store;
 mod worker;
 mod worker_pool;
 
-pub use current_task::{CurrentTask, CurrentTaskError};
+pub use models::current_task::{CurrentTask, CurrentTaskError};
+pub use models::task::{Task, TaskExecError};
+pub use models::task_state::TaskState;
 pub use queue_config::QueueConfig;
 pub use stores::SqliteTaskStore;
-pub use task::{Task, TaskExecError};
 pub use task_instance_builder::TaskInstanceBuilder;
 pub use task_like::{TaskLike, TaskLikeExt};
-pub use task_state::TaskState;
 pub use task_store::{TaskStore, TaskStoreError, TaskStoreMetrics};
 pub use worker::{Worker, WorkerError};
 pub use worker_pool::{ExecuteTaskFn, StateFn, WorkerPool, WorkerPoolError};
 
 pub mod tests {
-    pub use current_task::tests::{default_current_task, increment_current_task_attempt_count};
     pub use task_like::tests::TestTask;
     pub use task_store::tests::default_task_store_metrics;
 
-    use super::{current_task, task_like, task_store};
+    use super::{task_like, task_store};
+    pub use crate::models::current_task;
+    pub use crate::models::current_task::tests::{
+        default_current_task, increment_current_task_attempt_count,
+    };
 }
 
 pub const MAXIMUM_CHECK_DELAY: Duration = Duration::from_millis(250);
