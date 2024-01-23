202f45ea9b75d36f2937cca140272578af7dc460
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2024-01-23 13:32:02 -0500

    fix: fix tests and operations for adjusted schema

--- crates/banyan-core-service/migrations/20240105162430_add_updated_at_and_deleted_at_columns_to_buckets_table.up.sql
+++ crates/banyan-core-service/migrations/20240105162430_add_updated_at_and_deleted_at_columns_to_buckets_table.up.sql
@@ -1,4 +1,4 @@
 ALTER TABLE buckets ADD COLUMN deleted_at TIMESTAMP;
 ALTER TABLE buckets ADD COLUMN updated_at TIMESTAMP;
 
-UPDATE buckets SET updated_at = created_at;
+UPDATE buckets SET updated_at = CURRENT_TIMESTAMP;
