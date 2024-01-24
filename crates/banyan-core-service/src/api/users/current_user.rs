9873e85d5a2fb72d95be284ab6f9d79027b30e51
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2024-01-24 10:35:26 -0500

    fix: update current user handler to use better error handling

--- crates/banyan-core-service/src/api/users/current_user.rs
+++ crates/banyan-core-service/src/api/users/current_user.rs
@@ -7,29 +7,41 @@ use crate::app::AppState;
 use crate::database::models::User;
 use crate::extractors::UserIdentity;
 
-pub async fn handler(user_identity: UserIdentity, State(state): State<AppState>) -> Response {
+pub async fn handler(
+    user_identity: UserIdentity,
+    State(state): State<AppState>,
+) -> Result<Response, CurrentUserError> {
     let database = state.database();
-    let mut conn = match database.acquire().await {
-        Ok(conn) => conn,
-        Err(err) => {
-            tracing::error!("failed to acquire database connection: {err}");
-            let err_msg = serde_json::json!({"msg": "backend service experienced an issue servicing the request"});
-            return (StatusCode::INTERNAL_SERVER_ERROR, Json(err_msg)).into_response();
-        }
-    };
-
+    let mut conn = database.acquire().await?;
     let user_id = user_identity.id().to_string();
 
-    match User::find_by_id(&mut conn, &user_id).await {
-        Ok(Some(u)) => (StatusCode::OK, Json(ApiUser::from(u))).into_response(),
-        Ok(None) => {
-            let err_msg = serde_json::json!({"msg": "not found"});
-            (StatusCode::NOT_FOUND, Json(err_msg)).into_response()
-        }
-        Err(err) => {
-            tracing::error!("failed to lookup user: {err}");
-            let err_msg = serde_json::json!({"msg": "backend service experienced an issue servicing the request"});
-            (StatusCode::INTERNAL_SERVER_ERROR, Json(err_msg)).into_response()
+    match User::find_by_id(&mut conn, &user_id).await? {
+        Some(u) => Ok((StatusCode::OK, Json(ApiUser::from(u))).into_response()),
+        None => Err(CurrentUserError::NotFound),
+    }
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum CurrentUserError {
+    #[error("an error occurred querying the database: {0}")]
+    DatabaseFailure(#[from] sqlx::Error),
+
+    #[error("current user doesn't exist?")]
+    NotFound,
+}
+
+impl IntoResponse for CurrentUserError {
+    fn into_response(self) -> Response {
+        match self {
+            CurrentUserError::DatabaseFailure(_) => {
+                tracing::error!("{self}");
+                let err_msg = serde_json::json!({"msg": "backend service experienced an issue servicing the request"});
+                (StatusCode::INTERNAL_SERVER_ERROR, Json(err_msg)).into_response()
+            }
+            CurrentUserError::NotFound => {
+                let err_msg = serde_json::json!({"msg": "not found"});
+                (StatusCode::NOT_FOUND, Json(err_msg)).into_response()
+            }
         }
     }
 }
