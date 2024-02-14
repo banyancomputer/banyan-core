32e55693d04c9b13ae534a475763bb240885b7c4
Vera Gonzalez <me@vera.lgbt>
2024-02-14 11:28:47 -0500

    feat: updated host insertion scripts

--- bin/add_staging_host.sh
+++ bin/add_staging_host.sh
@@ -3,11 +3,12 @@
 export STAGING_HOST_PUBKEY="$(cat crates/banyan-staging-service/data/service-key.public)"
 export STAGING_HOST_FINGERPRINT="$(cat crates/banyan-staging-service/data/service-key.fingerprint)"
 export STAGING_HOST_NAME="banyan-staging"
+export STAGING_HOST_REGION="North America"
 export STAGING_HOST_URL="http://127.0.0.1:3002/"
 export STAGING_HOST_BYTE_LIMIT="549755813888000"
 
 cat <<ESQL | sqlite3 ./crates/banyan-core-service/data/server.db || fail 4 "creating staging server storage host record"
 INSERT INTO storage_hosts
-  (name, url, used_storage, available_storage, fingerprint, pem)
-  VALUES ('${STAGING_HOST_NAME}', '${STAGING_HOST_URL}', 0, ${STAGING_HOST_BYTE_LIMIT}, '${STAGING_HOST_FINGERPRINT}', '${STAGING_HOST_PUBKEY}');
+  (name, url, used_storage, reserved_storage, available_storage, region, fingerprint, pem)
+  VALUES ('${STAGING_HOST_NAME}', '${STAGING_HOST_URL}', 0, 0, ${STAGING_HOST_BYTE_LIMIT}, '${STAGING_HOST_REGION}', '${STAGING_HOST_FINGERPRINT}', '${STAGING_HOST_PUBKEY}');
 ESQL
--- crates/banyan-core-service/bin/add_storage_host.sh
+++ crates/banyan-core-service/bin/add_storage_host.sh
@@ -1,11 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-export STORAGE_HOST_PUBKEY=$(cat ../banyan-staging-service/data/service-key.public)
-export STORAGE_HOST_FINGERPRINT=$(cat ../banyan-staging-service/data/service-key.fingerprint)
+export STORAGE_HOST_PUBKEY="$(cat ../banyan-storage-provider-service/data/service-key.public)"
+export STORAGE_HOST_FINGERPRINT="$(cat ../banyan-storage-provider-service/data/service-key.fingerprint)"
+export STORAGE_HOST_NAME="banyan-storage-provider"
+export STORAGE_HOST_REGION="North America"
+export STORAGE_HOST_URL="http://127.0.0.1:3003/"
+export STORAGE_HOST_BYTE_LIMIT="549755813888000"
 
-cat <<EOSQL | sqlite3 ./data/server.db
-DELETE FROM storage_hosts;
-INSERT INTO storage_hosts (name, url, used_storage, reserved_storage, available_storage, region, fingerprint, pem)
-  VALUES ('banyan-staging', 'http://127.0.0.1:3002/', 0, 0, 549755813888000, 'North America', '${STORAGE_HOST_FINGERPRINT}', '${STORAGE_HOST_PUBKEY}')
-  RETURNING id;
-EOSQL
+cat <<ESQL | sqlite3 ./data/server.db || fail 7 "creating storage server storage host record"
+INSERT INTO storage_hosts
+  (name, url, used_storage, reserved_storage, available_storage, region, fingerprint, pem)
+  VALUES ('${STORAGE_HOST_NAME}', '${STORAGE_HOST_URL}', 0, 0, ${STORAGE_HOST_BYTE_LIMIT}, '${STORAGE_HOST_REGION}', '${STORAGE_HOST_FINGERPRINT}', '${STORAGE_HOST_PUBKEY}');
+ESQL
