dd4ea05ea3e88a438be6b22384f59f6b1ca49699
Vera Gonzalez <me@vera.lgbt>
2024-02-20 10:01:24 -0500

    feat: updated query

--- crates/banyan-core-service/migrations/20240215181835_rename_soft_deleted_buckets.up.sql
+++ crates/banyan-core-service/migrations/20240215181835_rename_soft_deleted_buckets.up.sql
@@ -4,5 +4,6 @@ DROP INDEX idx_buckets_on_unique_user_id_and_name;
 -- Note that we IFNULl the deleted_at column to maintain the unique constraint
 -- that non-deleted buckets must be unique across user_id and name.
 CREATE UNIQUE INDEX idx_buckets_on_unique_user_id_and_name_and_deleted_at
-  ON buckets(user_id, name, IFNULL(deleted_at, 0));
+  ON buckets(user_id, name)
+  WHERE deleted_at IS NULL;
 
