91de77dcc8759caf98275a9fb47e691e029b0105
Plamen Hristov <phristov@blocksoft.ltd>
2024-02-28 09:53:09 +0200

    Fixed unique constraint for storage metrics

---
+++ crates/banyan-core-service/migrations/20240228074146_alter_metrics_table_unique_constraint.up.sql
@@ -0,0 +1,3 @@
+-- Add up migration script here
+DROP INDEX idx_metrics_traffic_slot_user_id;
+CREATE UNIQUE INDEX idx_metrics_traffic_slot_user_id_storage_host_id ON metrics_traffic (slot, storage_host_id, user_id);
