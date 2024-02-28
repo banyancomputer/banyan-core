f458c5b01fe4b5cedd6ea4b0cd946d00e7d0f2f3
Plamen Hristov <phristov@blocksoft.ltd>
2024-02-28 09:55:56 +0200

    Removed comment from migration script

--- crates/banyan-core-service/migrations/20240228074146_alter_metrics_table_unique_constraint.up.sql
+++ crates/banyan-core-service/migrations/20240228074146_alter_metrics_table_unique_constraint.up.sql
@@ -1,3 +1,2 @@
--- Add up migration script here
 DROP INDEX idx_metrics_traffic_slot_user_id;
 CREATE UNIQUE INDEX idx_metrics_traffic_slot_user_id_storage_host_id ON metrics_traffic (slot, storage_host_id, user_id);
