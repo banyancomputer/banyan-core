dc09ffb67df5bdd0f75caaaaf8b34861de1b9a06
Plamen Hristov <phristov@blocksoft.ltd>
2024-03-03 16:20:27 +0200

    Fixed migration script

--- crates/banyan-core-service/migrations/20240301112013_add_metrics_storage_table.up.sql
+++ crates/banyan-core-service/migrations/20240301112013_add_metrics_storage_table.up.sql
@@ -10,4 +10,4 @@ CREATE TABLE metrics_storage
 CREATE INDEX idx_metrics_storage_user_id ON metrics_storage (user_id);
 CREATE INDEX idx_metrics_storage_storage_host_id ON metrics_storage (storage_host_id);
 CREATE INDEX idx_metrics_storage_slot ON metrics_storage (slot);
-CREATE UNIQUE INDEX idx_metrics_storage_slot_user_id_storage_host_id ON metrics_traffic (slot, user_id, storage_host_id);
+CREATE UNIQUE INDEX idx_metrics_storage_slot_user_id_storage_host_id ON metrics_storage (slot, user_id, storage_host_id);
