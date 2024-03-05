b1bc7e9607f8cda4920b614590e5a1e56f70ae02
Vera Gonzalez <me@vera.lgbt>
2024-03-05 04:44:32 -0500

    feat: added publickey endpoint on core

--- crates/banyan-core-service/src/health_check/mod.rs
+++ crates/banyan-core-service/src/health_check/mod.rs
@@ -10,6 +10,7 @@ use tower_http::limit::RequestBodyLimitLayer;
 
 mod data_source;
 mod liveness;
+mod publickey;
 mod readiness;
 mod version;
 
@@ -34,6 +35,7 @@ where
         .route("/healthz", get(liveness::handler))
         .route("/readyz", get(readiness::handler))
         .route("/version", get(version::handler))
+        .route("/publickey", get(publickey::handler))
         .layer(cors_layer)
         .layer(RequestBodyLimitLayer::new(HEALTHCHECK_REQUEST_SIZE_LIMIT))
         .with_state(state)
