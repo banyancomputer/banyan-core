0ec3e979b610b989c3ee32595d9d39b9abc87f97
Vera Gonzalez <me@vera.lgbt>
2024-03-06 07:47:37 -0500

    fix: fmt

--- crates/banyan-core-service/src/database/models/user.rs
+++ crates/banyan-core-service/src/database/models/user.rs
@@ -114,7 +114,6 @@ impl User {
         .await
         .map(|_| {
             self.consumed_tokens += tokens_used;
-            
         })
     }
 
@@ -136,7 +135,6 @@ impl User {
         .await
         .map(|_| {
             self.earned_tokens += tokens_earned;
-            
         })
     }
 
