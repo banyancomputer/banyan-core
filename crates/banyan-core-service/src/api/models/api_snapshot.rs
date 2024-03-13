a726b5e6770bc42ccc015673735ae26714641126
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2024-03-13 11:03:29 -0400

    feat: also include bucket IDs in returned snapshot data

--- crates/banyan-core-service/src/api/models/api_snapshot.rs
+++ crates/banyan-core-service/src/api/models/api_snapshot.rs
@@ -5,22 +5,26 @@ use crate::database::models::Snapshot;
 #[derive(Serialize)]
 pub struct ApiSnapshot {
     pub id: String,
+    pub bucket_id: String,
     pub metadata_id: String,
-    pub created_at: i64,
 
     #[serde(skip_serializing_if = "Option::is_none")]
     pub size: Option<i64>,
-
     pub state: String,
+
+    pub created_at: i64,
 }
 
 impl From<Snapshot> for ApiSnapshot {
     fn from(val: Snapshot) -> Self {
         Self {
             id: val.id,
+            bucket_id: val.bucket_id,
             metadata_id: val.metadata_id,
+
             size: val.size,
             state: val.state.to_string(),
+
             created_at: val.created_at.unix_timestamp(),
         }
     }
--- crates/banyan-core-service/src/database/models/snapshot.rs
+++ crates/banyan-core-service/src/database/models/snapshot.rs
@@ -3,6 +3,7 @@ use time::OffsetDateTime;
 #[derive(Debug, sqlx::FromRow)]
 pub struct Snapshot {
     pub id: String,
+    pub bucket_id: String,
     pub metadata_id: String,
     pub state: String,
     pub size: Option<i64>,
