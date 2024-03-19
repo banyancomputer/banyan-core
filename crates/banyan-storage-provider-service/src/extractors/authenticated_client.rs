d1bfb4469246085e4ffbcf5d85b1d49672130363
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2024-03-19 18:19:18 -0400

    feat: remove audience checks

--- crates/banyan-storage-provider-service/src/extractors/authenticated_client.rs
+++ crates/banyan-storage-provider-service/src/extractors/authenticated_client.rs
@@ -96,9 +96,6 @@ where
 
         let verification_options = VerificationOptions {
             accept_future: false,
-            allowed_audiences: Some(HashSet::from_strings(&[
-                PlatformName::from_ref(state).to_string()
-            ])),
             max_validity: Some(Duration::from_secs(MAXIMUM_TOKEN_AGE)),
             time_tolerance: Some(Duration::from_secs(15)),
             ..Default::default()
