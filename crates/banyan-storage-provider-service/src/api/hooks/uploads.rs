48553b98c4cfaf328987a5532f7c06ff4430cd42
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2024-03-22 15:10:29 -0400

    feat: work on removing cid for the storage provider

--- crates/banyan-storage-provider-service/src/api/hooks/uploads.rs
+++ crates/banyan-storage-provider-service/src/api/hooks/uploads.rs
@@ -5,9 +5,8 @@ use axum::response::{IntoResponse, Response};
 use axum::{Json, TypedHeader};
 use jwt_simple::prelude::{Deserialize, Serialize};
 
-use crate::api::upload::{start_upload, Upload, UploadError};
 use crate::app::AppState;
-use crate::database::models::AuthorizedStorage;
+use crate::database::models::{AuthorizedStorage, Upload};
 use crate::extractors::PlatformIdentity;
 
 #[derive(Serialize, Deserialize)]
@@ -23,9 +22,10 @@ pub async fn handler(
     _: PlatformIdentity,
     TypedHeader(content_len): TypedHeader<ContentLength>,
     Json(request): Json<UploadRequest>,
-) -> Result<Response, UploadError> {
+) -> Result<Response, UploadRequestError> {
     let db = state.database();
     let reported_body_length = content_len.0;
+
     AuthorizedStorage::create_if_missing(
         &db,
         &request.client_id,
@@ -50,4 +50,18 @@ pub async fn handler(
 
     let msg = serde_json::json!({"upload_id": upload.id});
     Ok((StatusCode::OK, Json(msg)).into_response())
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum UploadRequestError {
+    #[error("database failure: {0}")]
+    Database(#[from] sqlx::Error),
+}
+
+impl IntoResponse for UploadRequestError {
+    fn into_response(self) -> Response {
+        tracing::error!("{self}");
+        let err_msg = serde_json::json!({ "msg": "a backend service issue occurred" });
+        (StatusCode::INTERNAL_SERVER_ERROR, Json(err_msg)).into_response()
+    }
 }
