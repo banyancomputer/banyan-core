489a955e2e89250a2089661b1fd6312835d23c4d
vladdezhniuk <vladdezhniuk@gmail.com>
2024-04-01 15:24:16 +0300

    feat: added possibility to accepr terms fron entering escrow key layout

--- crates/banyan-core-service/frontend/src/app/pages/enterEncryptionKey.tsx
+++ crates/banyan-core-service/frontend/src/app/pages/enterEncryptionKey.tsx
@@ -1,15 +1,26 @@
 import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
 import { unwrapResult } from '@reduxjs/toolkit';
+import { useEffect, useState } from 'react';
 
 import { Input } from '@components/common/Input';
 import { PrimaryButton } from '@components/common/PrimaryButton';
 
 import { useAppDispatch, useAppSelector } from '@app/store';
 import { initializeKeystore } from '@store/keystore/actions';
+import { TermsAndColditionsClient } from '@/api/termsAndConditions';
+import { UserClient } from '@/api/user';
+
+import { Done } from '@static/images/common';
+
+const termsClient = new TermsAndColditionsClient();
+const userClient = new UserClient();
 
 const EnterEncryptionKey = () => {
     const messages = useAppSelector(state => state.locales.messages.pages.enterEncryptionKey);
+    const { agreeToTerms, termsOfService, and, privacyPolicy } = useAppSelector(state => state.locales.messages.pages.createEncryptionKey);
+    const [isTermsCheckboxVisible, setIsTermsCheckboxVisible] = useState(false);
+    const [areTermsAccepted, setAreTermsAccepted] = useState(false);
     const navigate = useNavigate();
     const dispatch = useAppDispatch();
     const {
@@ -23,7 +34,7 @@ const EnterEncryptionKey = () => {
         values: { keyphrase: '' },
     });
     const { keyphrase } = watch();
-    const isDataCorrect = !Object.keys(errors).length && keyphrase.length >= 8;
+    const isDataCorrect = !Object.keys(errors).length && keyphrase.length >= 8 && (isTermsCheckboxVisible ? areTermsAccepted : true);
 
     const confirm = async () => {
         try {
@@ -33,6 +44,16 @@ const EnterEncryptionKey = () => {
             setError('keyphrase', { message: `${messages.secretKeyError}` });
         };
     };
+
+    useEffect(() => {
+        (async () => {
+            const termsAndConditions = await termsClient.getTermsAndCondition();
+            const user = await userClient.getCurrentUser();
+            if (!user.acceptedTosAt || (user.acceptedTosAt <= +termsAndConditions.tos_date)) {
+                setIsTermsCheckboxVisible(true)
+            }
+        })();
+    }, [])
 
     return (
         <form
@@ -50,6 +71,38 @@ const EnterEncryptionKey = () => {
                     required: `${messages.encryptionKeyPlaceholder}`,
                 })}
             />
+            {isTermsCheckboxVisible ?
+                <div
+                    className="flex items-center gap-2"
+                >
+                    <div
+                        className="w-5 h-5 flex justify-center items-center border-1 border-border-darken rounded-sm text-termsAndConditions-activeAccountType cursor-pointer"
+                        onClick={() => setAreTermsAccepted(prev => !prev)}
+                    >
+                        {areTermsAccepted && <Done />}
+                    </div>
+                    <div className="flex items-center gap-1.5 text-[13px] font-medium whitespace-nowrap">
+                        {`${agreeToTerms}`}
+                        <a
+                            href="https://www.banyan.computer/terms-of-service"
+                            target="_blank"
+                            className="underline"
+                        >
+                            {`${termsOfService}`}
+                        </a>
+                        {`${and}`}
+                        <a
+                            href="https://www.banyan.computer/privacy-policy"
+                            target="_blank"
+                            className="underline"
+                        >
+                            {`${privacyPolicy}`}
+                        </a>
+                    </div>
+                </div>
+                :
+                null
+            }
             {/* <div className="flex items-center gap-1.5 text-[13px] font-medium whitespace-nowrap text-button-primary ">
                 {`${messages.forgotEncryptionKey}?`}
                 <button className="underline font-semibold hover:text-button-primaryHover">
