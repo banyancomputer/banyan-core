0d7f03266fc04399822d6c299287d4f80c477226
vladdezhniuk <vladdezhniuk@gmail.com>
2024-04-15 17:52:36 +0300

    fix: dark mode issues fixed

--- crates/banyan-core-service/frontend/src/app/static/images/common/index.tsx
+++ crates/banyan-core-service/frontend/src/app/static/images/common/index.tsx
@@ -360,7 +360,7 @@ export const ImageFileIcon = (params: SVGProps<any>) => <svg width="40" height="
 
 export const Spinner = (params: SVGProps<any>) => <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" {...params}>
     <path className="bg" d="M16 0a16 16 0 0 0 0 32 16 16 0 0 0 0-32m0 4a12 12 0 0 1 0 24 12 12 0 0 1 0-24" fill="#ffffff00" />
-    <path className="fg" d="M16 0a16 16 0 0 1 16 16h-4A12 12 0 0 0 16 4V0zm0 32A16 16 0 0 1 0 16h4a12 12 0 0 0 12 12v4z" fill="#currentColor" />
+    <path className="fg" d="M16 0a16 16 0 0 1 16 16h-4A12 12 0 0 0 16 4V0zm0 32A16 16 0 0 1 0 16h4a12 12 0 0 0 12 12v4z" fill="currentColor" />
 </svg>;
 
 export const SearchIcon = (params: SVGProps<any>) => <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" {...params}>
