46751da1065c6bebe1700b8c5b9f82a02f2d4cfa
vladdezhniuk <vladdezhniuk@gmail.com>
2024-04-22 16:52:29 +0300

    fix: console removed

--- crates/banyan-core-service/frontend/src/app/utils/clickHandlers.ts
+++ crates/banyan-core-service/frontend/src/app/utils/clickHandlers.ts
@@ -1,8 +1,6 @@
 /** Will be used for modals and context menus to handle closing */
 export const popupClickHandler = (ref: HTMLDivElement, onchange:(visible: boolean) => void) =>
     function(this: Document, event: MouseEvent) {
-        console.log('ref', !ref.contains(event.target as Node));
-        
         if (!ref) {
             return;
         };
